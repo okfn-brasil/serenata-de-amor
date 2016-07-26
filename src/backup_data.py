f29e2953d539bb6fb770f7b42cbb2b81f086ec54
Irio Musskopf <iirineu@gmail.com>
2016-07-26 13:58:39 -0300

    New scripts for making backup of datasets from government site

---
+++ src/backup_data.py
@@ -0,0 +1,20 @@
+import configparser
+import datetime
+import os
+import tinys3
+
+settings = configparser.RawConfigParser()
+settings.read('config.ini')
+access_key = settings.get('Amazon', 'AccessKey')
+secret_key = settings.get('Amazon', 'SecretKey')
+
+connection = tinys3.Connection(access_key,
+                               secret_key,
+                               default_bucket='serenata-de-amor-data',
+                               endpoint='s3-sa-east-1.amazonaws.com')
+
+date = datetime.datetime.utcnow().isoformat()[:10]
+files = [name for name in os.listdir('data') if not name.startswith('.')]
+for filename in files:
+    backup_filename = '%s-%s' % (date, filename)
+    connection.upload(backup_filename, open('data/' + filename, 'rb'))
