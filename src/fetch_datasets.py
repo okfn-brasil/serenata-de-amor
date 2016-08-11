2892180202fc6f8118d4ba01363c23bf2439360c
Irio Musskopf <iirineu@gmail.com>
2016-08-11 19:46:15 -0300

    Download backed up datasets on setup script

--- src/fetch_datasets.py
+++ src/fetch_datasets.py
@@ -1,16 +1,47 @@
+import configparser
+from optparse import OptionParser
 import subprocess
 from urllib.request import urlretrieve
 
-dataset_urls = [
-    'http://www.camara.gov.br/cotas/AnoAtual.zip',
-    'http://www.camara.gov.br/cotas/AnoAnterior.zip',
-    'http://www.camara.gov.br/cotas/AnosAnteriores.zip']
-filenames = map(lambda url: url.split('/')[-1], dataset_urls)
+def download_source():
+    dataset_urls = [
+        'http://www.camara.gov.br/cotas/AnoAtual.zip',
+        'http://www.camara.gov.br/cotas/AnoAnterior.zip',
+        'http://www.camara.gov.br/cotas/AnosAnteriores.zip']
+    filenames = map(lambda url: url.split('/')[-1], dataset_urls)
 
-for url, filename in zip(dataset_urls, filenames):
-    filepath = 'data/%s' % filename
-    urlretrieve(url, filepath)
-    subprocess.call(['unzip', '-o', filepath, '-d', 'data'])
+    for url, filename in zip(dataset_urls, filenames):
+        filepath = 'data/%s' % filename
+        urlretrieve(url, filepath)
+        subprocess.call(['unzip', '-o', filepath, '-d', 'data'])
 
-urlretrieve('http://www2.camara.leg.br/transparencia/cota-para-exercicio-da-atividade-parlamentar/explicacoes-sobre-o-formato-dos-arquivos-xml',
-            'data/datasets_format.html')
+    urlretrieve('http://www2.camara.leg.br/transparencia/cota-para-exercicio-da-atividade-parlamentar/explicacoes-sobre-o-formato-dos-arquivos-xml',
+                'data/datasets_format.html')
+
+def download_backup():
+    settings = configparser.RawConfigParser()
+    settings.read('config.ini')
+    bucket = settings.get('Amazon', 'Bucket')
+    region = settings.get('Amazon', 'Region')
+
+    files = ['2016-08-08-AnoAtual.csv',
+             '2016-08-08-AnoAnterior',
+             '2016-08-08-AnosAnteriores.csv',
+             '2016-08-08-datasets_format.html']
+    for filename in files:
+        url = 'https://%s.amazonaws.com/%s/%s' % (region, bucket, filename)
+        filepath = 'data/%s' % filename
+        urlretrieve(url, filepath)
+
+
+
+parser = OptionParser(usage='usage: %prog [options]')
+parser.add_option('--from-source',
+                  action='store_true',
+                  help='download files as provided by original sources')
+
+(options, args) = parser.parse_args()
+if options.from_source:
+    download_source()
+else:
+    download_backup()
