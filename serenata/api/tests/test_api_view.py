4cbfd50f0cedb27a52199fe746f05993b654eea6
Eduardo Cuducos <cuducos@gmail.com>
2016-09-05 19:10:16 -0300

    Clean up

--- serenata/api/tests/test_api_view.py
+++ serenata/api/tests/test_api_view.py
@@ -10,51 +10,51 @@ class TestGet(TestCase):
 
     def setUp(self):
         Document.objects.create(
-            document_id=5621548,
-            congressperson_name='ADELMO LEÃO',
-            congressperson_id=74752,
-            congressperson_document=248,
-            term=2015,
-            state='MG',
-            party='PP',
-            term_id=55,
-            subquota_number=11,
-            subquota_description='Postal services',
-            subquota_group_id=0,
-            subquota_group_description='NaN',
-            supplier='Magnino Franquia e Serviços Ltda',
-            cnpj_cpf='20574089000107',
-            document_number=29022,
-            document_type=1,
-            issue_date='2015-02-26 00:00:00',
-            document_value=190.05,
-            remark_value=0,
-            net_value=190.05,
-            month=2,
-            year=2015,
-            installment=0,
-            passenger='NaN',
-            leg_of_the_trip=0,
-            batch_number=1172687,
-            reimbursement_number=4953,
-            reimbursement_value=0,
-            applicant_id=705,
+            document_id=42,
+            congressperson_name='Roger That',
+            congressperson_id=1,
+            congressperson_document=2,
+            term=1970,
+            state='UF',
+            party='Partido',
+            term_id=3,
+            subquota_number=4,
+            subquota_description='Subquota description',
+            subquota_group_id=5,
+            subquota_group_description='Subquota group description',
+            supplier='Acme',
+            cnpj_cpf='11111111111111',
+            document_number=6,
+            document_type=7,
+            issue_date='1970-01-01 00:00:00',
+            document_value=8.90,
+            remark_value=1.23,
+            net_value=4.56,
+            month=1,
+            year=1970,
+            installment=7,
+            passenger='John Doe',
+            leg_of_the_trip=8,
+            batch_number=9,
+            reimbursement_number=10,
+            reimbursement_value=11.12,
+            applicant_id=13
         )
-        self.resp = self.client.get(resolve_url('api:document', 5621548))
+        self.resp = self.client.get(resolve_url('api:document', 42))
 
     def test_status_code(self):
         self.assertEqual(200, self.resp.status_code)
 
     def test_content(self):
         content = json.loads(self.resp.content.decode('utf-8'))
-        self.assertEqual('ADELMO LEÃO', content['congressperson_name'])
-        self.assertEqual(190.05, content['net_value'])
+        self.assertEqual('Roger That', content['congressperson_name'])
+        self.assertEqual(4.56, content['net_value'])
 
 
 class Test404(TestCase):
 
     def setUp(self):
-        self.resp = self.client.get(resolve_url('api:document', 5621548))
+        self.resp = self.client.get(resolve_url('api:document', 42))
 
     def test_status_code(self):
         self.assertEqual(404, self.resp.status_code)
