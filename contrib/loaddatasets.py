894eb102545c5a5edb20b9a5bab5fe2375628d57
Eduardo Cuducos <cuducos@gmail.com>
2016-09-06 18:48:16 -0300

    Add start_from_round option

--- contrib/loaddatasets.py
+++ contrib/loaddatasets.py
@@ -9,6 +9,7 @@ optional arguments to sequentially load 1000 records during each execution.
 from itertools import chain
 from math import ceil
 from subprocess import call
+from sys import argv
 from time import sleep
 
 TOTAL = 2072729
@@ -16,19 +17,25 @@ PER_EXECUTION = 1000
 ROUNDS = ceil(TOTAL / PER_EXECUTION)
 COMMAND = ('heroku', 'run', 'python', 'manage.py', 'loaddatasets')
 
+try:
+    START_FROM_ROUND = int(argv[1])
+except IndexError:
+    START_FROM_ROUND = 1
+
 
 for count in range(ROUNDS):
-    start_at = (PER_EXECUTION * count) + 1
-    start = ('--start', str(start_at))
-    limit = ('--limit', str(PER_EXECUTION))
+    if (count + 1) > (START_FROM_ROUND - 1):
+        start_at = (PER_EXECUTION * count) + 1
+        start = ('--start', str(start_at))
+        limit = ('--limit', str(PER_EXECUTION))
 
-    msg1 = '==> Round {} of {}'.format(count + 1, ROUNDS)
-    print(msg1)
+        msg1 = '==> Round {} of {}'.format(count + 1, ROUNDS)
+        print(msg1)
 
-    msg2 = '    Staring at {} and loading {} records'
-    print(msg2.format(start_at, PER_EXECUTION))
+        msg2 = '    Staring at {} and loading {} records'
+        print(msg2.format(start_at, PER_EXECUTION))
 
-    call(chain(COMMAND, start, limit))
+        call(chain(COMMAND, start, limit))
 
-    print('==> Sleeping for 1min                                             ')
-    sleep(60)
+        print('==> Sleeping for 1min                                             ')
+        sleep(60)
