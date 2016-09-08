a5e9dba4d0d0e765f77ef3c3f182b62d6cdd0cbd
Eduardo Cuducos <cuducos@gmail.com>
2016-09-08 11:43:53 -0300

    Separate operations from objects in fetch receipts script

--- src/fetch_receipts.py
+++ src/fetch_receipts.py
@@ -2,7 +2,6 @@ import os
 import re
 import sys
 from argparse import ArgumentParser, RawTextHelpFormatter
-from collections import namedtuple
 from urllib.error import HTTPError
 from urllib.request import urlretrieve
 
@@ -10,33 +9,29 @@ from humanize import naturalsize
 import numpy as np
 import pandas as pd
 
-Receipt = namedtuple('Receipt', ['path', 'url'])
 
-
-class FetchReceipts:
+class Receipts:
 
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     DATA_DIR = os.path.join(BASE_DIR, 'data')
     REGEX = r'^[\d-]{11}(current-year|last-year|previous-years).xz$'
 
-    def __init__(self, target, limit=None):
+    def __call__(self):
+        return self.receipts
+
+    def datasets(self):
+        """List generator with the full path of each CSV/dataset."""
+        for file_name in os.listdir(self.DATA_DIR):
+            if re.compile(self.REGEX).match(file_name):
+                yield os.path.join(self.DATA_DIR, file_name)
+
+    def all(self):
         """
-        :param target: (str) path to the directory where images will be saved
-        :param limit: (int) limit the amount of files to save
+        List generator with Receipt named tuples containing the path of the
+        receipt image (to be used when saving it, for example) and the URL of
+        the receipt at the Lower House servers.
         """
-
-        # check if target directory exists
-        if not os.path.exists(target):
-            raise RuntimeError('Directory {} does not exist'.format(target))
-            sys.exit()
-
-        # check if target directory is a directory (not a file)
-        if not os.path.isdir(target):
-            raise RuntimeError('{} is a file, not a directory'.format(target))
-            sys.exit()
-
-        # dtype to be used by pandas (inside self.parse)
-        self.dtype = {
+        dtype = {
             'document_id': np.str,
             'congressperson_id': np.str,
             'congressperson_document': np.str,
@@ -44,104 +39,111 @@ class FetchReceipts:
             'cnpj_cpf': np.str,
             'reimbursement_number': np.str
         }
-
-        # save receipts
-        self.limit = limit
-        self.target = target
-        self.progress = {
-            'count': 0,
-            'size': 0,
-            'errors': list(),
-            'skipped': list()
-        }
-        for receipt in self.receipts:
-            if not self.limit or self.progress['count'] < self.limit:
-                self.save(receipt)
-            else:
-                break
-
-        # print errors
-        if self.progress['errors']:
-            errors = self.progress['errors']
-            print('\n==> {} receipts could not be saved:'.format(len(errors)))
-            for index, url in enumerate(errors):
-                print('    {}. {}'.format(index + 1, url))
-
-        # print skipped files (already existing)
-        if self.progress['skipped']:
-            skipped = self.progress['skipped']
-            msg = '\n==> {} receipts were skipped, probably they already exist'
-            print(msg.format(len(skipped)))
-            for index, url in enumerate(skipped):
-                print('    {}. {}'.format(index + 1, url))
+        for dataset in self.datasets():
+            data = pd.read_csv(dataset, parse_dates=[16], dtype=dtype)
+            yield from(Receipt(row) for row in data.itertuples())
 
 
-    @property
-    def datasets(self):
-        """List generator with the full path of each CSV/dataset."""
-        for file_name in os.listdir(self.DATA_DIR):
-            if re.compile(self.REGEX).match(file_name):
-                yield os.path.join(self.DATA_DIR, file_name)
+class Receipt:
 
-    @property
-    def receipts(self):
+    def __init__(self, receipt):
         """
-        List generator with Receipt named tuples containing the path of the
-        receipt image (to be used when saving it, for example) and the URL of
-        the receipt at the Lower House servers.
+        :param receipt: a Pandas DataFrame row as a NamedTuple (see
+        `itertuples` method)
         """
-        for dataset in self.datasets:
-            data = pd.read_csv(dataset, parse_dates=[16], dtype=self.dtype)
-            for row in data.itertuples():
-                yield Receipt(self.receipt_path(row), self.receipt_url(row))
+        self.receipt = receipt
 
-    def save(self, receipt):
-        """Get a receipt named tuple and save it to the target directory"""
-        os.makedirs(os.path.dirname(receipt.path), exist_ok=True)
-        if not os.path.exists(receipt.path):
-            try:
-                file_name, header = urlretrieve(receipt.url, receipt.path)
-                self.progress['count'] += 1
-                self.progress['size'] += int(header['Content-Length'])
-                raw_msg = '==> Downloaded {} files ({})                       '
-                msg = raw_msg.format(
-                    self.progress['count'],
-                    naturalsize(self.progress['size'])
-                )
-                print(msg, end='\r')
-            except HTTPError:
-                self.progress['errors'].append(receipt.url)
-        else:
-            self.progress['skipped'].append(receipt.url)
-
-    def receipt_path(self, row):
+    def path(self, target):
         """
-        Given a Pandas DataFrame row as a NamedTuple (see `itertuples` method),
-        it returns the absolute path to the receipt, path to be used when
-        saving the file.
+        Given a target directory (string), it returns the absolute path to the
+        receipt (the path to be used when saving the file).
         """
         return os.path.join(
-            os.path.abspath(self.target),
-            str(row.applicant_id),
-            str(row.year),
-            str(row.document_id) + '.pdf'
+            os.path.abspath(target),
+            str(self.receipt.applicant_id),
+            str(self.receipt.year),
+            str(self.receipt.document_id) + '.pdf'
         )
 
-    @staticmethod
-    def receipt_url(row):
+    @property
+    def url(self):
         """
-        Given a Pandas DataFrame row as a NamedTuple (see `itertuples` method),
-        it returns the URL of this receipt at the Lower House server.
+        Returns the URL of this receipt at the Lower House server.
         """
         base = 'http://www.camara.gov.br/cota-parlamentar/documentos/publ/'
         recipe = '{base}{applicant_id}/{year}/{document_id}.pdf'
         return recipe.format(
             base=base,
-            applicant_id=row.applicant_id,
-            year=row.year,
-            document_id=row.document_id
+            applicant_id=self.receipt.applicant_id,
+            year=self.receipt.year,
+            document_id=self.receipt.document_id
         )
 
+
+def run(target, limit=None):
+    """
+    :param target: (string) path to the directory to save the receipts
+    :param limit: (int) limit the amount of receipts to fecth
+    """
+    target = target
+    limit = limit
+    progress = {
+        'count': 0,
+        'size': 0,
+        'errors': list(),
+        'skipped': list()
+    }
+
+    # check if target directory exists
+    if not os.path.exists(target):
+        raise RuntimeError('Directory {} does not exist'.format(target))
+        sys.exit()
+
+    # check if target directory is a directory (not a file)
+    if not os.path.isdir(target):
+        raise RuntimeError('{} is a file, not a directory'.format(target))
+        sys.exit()
+
+    # save receipts
+    for receipt in Receipts().all():
+        path = receipt.path(target)
+        if not limit or progress['count'] < limit:
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            if not os.path.exists(path):
+                try:
+                    file_name, header = urlretrieve(receipt.url, path)
+                    progress['count'] += 1
+                    progress['size'] += int(header['Content-Length'])
+                    raw_msg = '==> Downloaded {:,} files ({})                 '
+                    msg = raw_msg.format(
+                        progress['count'],
+                        naturalsize(progress['size'])
+                    )
+                    print(msg, end='\r')
+                except HTTPError:
+                    progress['errors'].append(receipt.url)
+            else:
+                progress['skipped'].append(receipt.url)
+        else:
+            break
+
+    # print summary
+    msg = '==> Downloaded {:,} files ({})                                     '
+    print(msg.format(progress['count'], naturalsize(progress['size'])))
+
+    # print errors
+    if progress['errors']:
+        msg = '\n==> {:,} receipts could not be saved:'
+        print(msg.format(len(progress['errors'])))
+        for index, url in enumerate(progress['errors']):
+            print('    {}. {}'.format(index + 1, url))
+
+    # print skipped files (already existing)
+    if progress['skipped']:
+        msg = '\n==> {:,} receipts were skipped (probably they already exist)'
+        print(msg.format(len(progress['skipped'])))
+        for index, url in enumerate(progress['skipped']):
+            print('    {}. {}'.format(index + 1, url))
 
 if __name__ == '__main__':
 
@@ -172,4 +174,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # run
-    FetchReceipts(args.target, args.limit)
+    run(args.target, args.limit)
