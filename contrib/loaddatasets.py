6a75ec12d58d45bc939cc249630fa476c8c95231
Eduardo Cuducos <cuducos@gmail.com>
2016-09-08 10:11:07 -0300

    Use bulk_insert to load datasets to database

--- contrib/loaddatasets.py
+++ contrib/loaddatasets.py
@@ -1,21 +1,19 @@
-
 #!/usr/bin/env python
 """
 The Django app from this repo has a command to load Serenata de Amor datasets
 to the app's database. As this is a huge amount of data to be loaded in one
 command (specially in production), this Python script runs the command with the
-optional arguments to sequentially load 1000 records during each execution.
+optional arguments to sequentially load 5,000 records during each execution.
 """
 from itertools import chain
 from math import ceil
 from subprocess import call
 from sys import argv
-from time import sleep
 
 TOTAL = 2072729
-PER_EXECUTION = 1000
+PER_EXECUTION = 5 * 1000
 ROUNDS = ceil(TOTAL / PER_EXECUTION)
-COMMAND = ('heroku', 'run', 'python', 'manage.py', 'loaddatasets')
+COMMAND = ('python', 'manage.py', 'loaddatasets')
 
 try:
     START_FROM_ROUND = int(argv[1])
@@ -28,14 +26,12 @@ for count in range(ROUNDS):
         start_at = (PER_EXECUTION * count) + 1
         start = ('--start', str(start_at))
         limit = ('--limit', str(PER_EXECUTION))
+        drop = ('-d',) if start_at == 1 else ()
 
-        msg1 = '==> Round {} of {}'.format(count + 1, ROUNDS)
+        msg1 = '\n==> Round {:,} of {:,}'.format(count + 1, ROUNDS)
         print(msg1)
 
-        msg2 = '    Staring at {} and loading {} records'
+        msg2 = '    Staring at {:,} and loading {:,} records'
         print(msg2.format(start_at, PER_EXECUTION))
 
-        call(chain(COMMAND, start, limit))
-
-        print('==> Sleeping for 1min                                             ')
-        sleep(60)
+        call(chain(COMMAND, start, limit, drop))
