5be28c31d5315d6f796012d84e69437675a751d7
pingfreud <felipe.c.argolo@gmail.com>
2016-09-11 22:42:28 -0300

    Starting Benford analysis notebook

--- develop/benford.R
+++ develop/benford.R
@@ -9,7 +9,7 @@ net_value.benford <- benford(data$net_value,number.of.digits = 2)
 MAD(net_value.benford)
 
 # Plot results. 
-plot(net_value.benford)
+plot(net_value.benford,)
 # Get 10 most frequent initial 2 digits and absolut different from benford distribution
 head(suspectsTable(net_value.benford),10) # Looks like 10 and 50 are favorite
 
@@ -19,3 +19,5 @@ suspects <- getSuspects(net_value.benford,data, how.many=2)
 # Here we have the most cited names 
 sort(table(suspects$congressperson_name)) #Looks like we have some suspects:)
 
+frequent_values <- suspectsTable(net_value.benford)
+ggplot(frequent_values,aes(y = absolute.diff, x = as.factor(digits))) + geom_point()
