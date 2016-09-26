cf4eddbc52817187c3dbdcad54af77f889ef97fe
Eduardo Cuducos <cuducos@gmail.com>
2016-09-26 16:37:00 -0300

    Add Supplier to model

--- jarbas/core/models.py
+++ jarbas/core/models.py
@@ -5,20 +5,20 @@ from requests import head
 class Document(models.Model):
 
     document_id = models.IntegerField('Document ID', db_index=True)
-    congressperson_name = models.CharField('Congressperson name', max_length=128)
+    congressperson_name = models.CharField('Congressperson name', max_length=33)
     congressperson_id = models.IntegerField('Congressperson ID', db_index=True)
     congressperson_document = models.IntegerField('Congressperson document')
     term = models.IntegerField('Term', db_index=True)
     state = models.CharField('State', max_length=2)
-    party = models.CharField('Party', db_index=True, max_length=16)
+    party = models.CharField('Party', db_index=True, max_length=7)
     term_id = models.IntegerField('Term ID')
     subquota_number = models.IntegerField('Subquote ID', db_index=True)
-    subquota_description = models.CharField('Subquota descrition', max_length=128)
+    subquota_description = models.CharField('Subquota descrition', max_length=56)
     subquota_group_id = models.IntegerField('Subquota group ID', db_index=True)
-    subquota_group_description = models.CharField('Subquota group description', max_length=128)
-    supplier = models.CharField('Supplier', max_length=128)
+    subquota_group_description = models.CharField('Subquota group description', max_length=20)
+    supplier = models.CharField('Supplier', max_length=100)
     cnpj_cpf = models.CharField('CNPJ or CPF', db_index=True, max_length=14)
-    document_number = models.CharField('Document number', max_length=128)
+    document_number = models.CharField('Document number', max_length=30)
     document_type = models.IntegerField('Document type', db_index=True)
     issue_date = models.DateTimeField('Issue date', blank=True, null=True)
     document_value = models.DecimalField('Document value', max_digits=10, decimal_places=3)
@@ -27,8 +27,8 @@ class Document(models.Model):
     month = models.IntegerField('Month', db_index=True)
     year = models.IntegerField('Year', db_index=True)
     installment = models.IntegerField('Installment')
-    passenger = models.CharField('Passenger', max_length=128)
-    leg_of_the_trip = models.CharField('Leg of the trip', max_length=128)
+    passenger = models.CharField('Passenger', max_length=59)
+    leg_of_the_trip = models.CharField('Leg of the trip', max_length=100)
     batch_number = models.IntegerField('Batch number')
     reimbursement_number = models.IntegerField('Reimbursement number', db_index=True)
     reimbursement_value = models.DecimalField('Reimbusrsement value', max_digits=10, decimal_places=3)
@@ -60,3 +60,43 @@ class Document(models.Model):
         self.save()
 
         return url
+
+
+class Activity(models.Model):
+
+    code = models.CharField('Code', max_length=10)
+    description = models.CharField('Description', max_length=167)
+
+
+class Supplier(models.Model):
+
+    cnpj = models.CharField('CNPJ', db_index=True, max_length=18)
+    opening = models.DateField('Opening date', blank=True, null=True)
+
+    legal_entity = models.CharField('Legal entity', blank=True, null=True, max_length=72)
+    trade_name = models.CharField('Trade name', blank=True, null=True, max_length=55)
+    name = models.CharField('Name', blank=True, null=True, max_length=144)
+    type = models.CharField('Type', blank=True, null=True, max_length=6)
+
+    main_activity = models.ManyToManyField(Activity, related_name='main')
+    secondary_activity = models.ManyToManyField(Activity, related_name='secondary')
+
+    status = models.CharField('Status', blank=True, null=True, max_length=5)
+    situation = models.CharField('Situation', blank=True, null=True, max_length=8)
+    situation_reason = models.CharField('Situation reason', blank=True, null=True, max_length=44)
+    situation_date = models.DateField('Situation date', blank=True, null=True)
+    special_situation = models.CharField('Special situation', blank=True, null=True, max_length=51)
+    special_situation_date = models.DateField('Special situation date', blank=True, null=True)
+    responsible_federative_entity = models.CharField('Responsible federative entity', blank=True, null=True, max_length=38)
+
+    address = models.CharField('Address', blank=True, null=True, max_length=64)
+    number = models.CharField('Number', blank=True, null=True, max_length=6)
+    additional_address_details = models.CharField('Additional address details', blank=True, null=True, max_length=143)
+    neighborhood = models.CharField('Neighborhood', blank=True, null=True, max_length=50)
+    zip_code = models.CharField('Zip code', blank=True, null=True, max_length=10)
+    city = models.CharField('City', blank=True, null=True, max_length=32)
+    state = models.CharField('State', blank=True, null=True, max_length=2)
+    email = models.EmailField('Email', blank=True, null=True, max_length=58)
+    phone = models.CharField('Phone', blank=True, null=True, max_length=32)
+
+    last_updated = models.DateTimeField('Last updated', blank=True, null=True)
--- jarbas/core/tests/__init__.py
+++ jarbas/core/tests/__init__.py
@@ -1,3 +1,6 @@
+from datetime import date, datetime
+
+
 sample_document_data = dict(
     document_id=42,
     congressperson_name='Roger That',
@@ -10,7 +13,7 @@ sample_document_data = dict(
     subquota_number=4,
     subquota_description='Subquota description',
     subquota_group_id=5,
-    subquota_group_description='Subquota group description',
+    subquota_group_description='Subquota group desc',
     supplier='Acme',
     cnpj_cpf='11111111111111',
     document_number='6',
@@ -28,4 +31,35 @@ sample_document_data = dict(
     reimbursement_number=10,
     reimbursement_value=11.12,
     applicant_id=13
+)
+
+sample_activity_data = dict(
+    code='42',
+    description='So long, so long, and thanks for all the fish'
+)
+
+sample_supplier_data = dict(
+    cnpj='12.345.678/9012-34',
+    opening=date(1995, 9, 27),
+    legal_entity='42 - The answer to life, the universe, and everything',
+    trade_name="Don't panic",
+    name='Do not panic, sir',
+    type='BOOK',
+    status='OK',
+    situation='EXISTS',
+    situation_reason='Douglas Adams wrote it',
+    situation_date=date(2016, 9, 25),
+    special_situation='WE LOVE IT',
+    special_situation_date=date(1997, 9, 28),
+    responsible_federative_entity='Vogons',
+    address='Earth',
+    number='',
+    additional_address_details='',
+    neighborhood='',
+    zip_code='',
+    city='',
+    state='',
+    email='',
+    phone='',
+    last_updated=datetime.now(),
 )
