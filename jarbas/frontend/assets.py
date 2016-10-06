7b5a4cd706a326d4e95051e1bdb1f26c01db268f
Eduardo Cuducos <cuducos@gmail.com>
2016-10-06 11:29:01 -0300

    Fix webassets `depends`

--- jarbas/frontend/assets.py
+++ jarbas/frontend/assets.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob
 
 from django.conf import settings
 from django_assets import Bundle, register
@@ -8,13 +9,13 @@ from webassets_elm import Elm
 register_filter(Elm)
 
 frontend_path = os.path.dirname(settings.ASSETS_ROOT)
+depends = glob(os.path.join(frontend_path, '**', '*.elm'), recursive=True)
 
 elm = Bundle(
     os.path.join(frontend_path, 'elm', 'Main.elm'),
     filters=('elm', 'rjsmin'),
-    depends=('**/*.elm'),
+    depends=depends,
     output='app.js'
 )
-
 
 register('elm', elm)
---
+++ jarbas/frontend/tests/test_bundle_dependecies.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from webassets.bundle import get_all_bundle_files
+
+from jarbas.frontend.assets import elm
+
+
+class TestDependencies(TestCase):
+
+    def test_dependencies(self):
+        files = set(get_all_bundle_files(elm))
+        self.assertEqual(8, len(files), files)
