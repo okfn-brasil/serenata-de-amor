1a083ca8646d55d98bf75e5e869e21edec85967a
Eduardo Cuducos <cuducos@gmail.com>
2016-11-11 09:43:50 -0200

    More tests for home view

--- jarbas/frontend/tests/test_home_view.py
+++ jarbas/frontend/tests/test_home_view.py
@@ -1,9 +1,27 @@
 from django.shortcuts import resolve_url
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 
 class TestGet(TestCase):
 
+    def setUp(self):
+        self.resp = self.client.get(resolve_url('home'))
+
     def test_status_code(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'frontend/home.html')
+
+    def test_contents(self):
+        expected = '<title>Jarbas | Serenata de Amor</title>'
+        self.assertIn(expected, self.resp.content.decode('utf-8'))
+
+    @override_settings(GOOGLE_STREET_VIEW_API_KEY=42)
+    def test_google_api_key(self):
         resp = self.client.get(resolve_url('home'))
-        self.assertEqual(200, resp.status_code)
+        expected = "googleStreetViewApiKey: '42'"
+        self.assertIn(expected, resp.content.decode('utf-8'))
+
+
+
