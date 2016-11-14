f0b6d52752ed4b661b0f5c89f8287b3189e7d542
Wisner Oliveira <wisner.oliveira@gmail.com>
2016-11-14 14:08:17 -0200

    Improvements and fixes

--- src/fetch_civil_names.py
+++ src/fetch_civil_names.py
@@ -7,95 +7,92 @@ import numpy as np
 from bs4 import BeautifulSoup
 
 
-DATA_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_PATH = os.path.join(DATA_BASE_DIR, 'data')
-DATE = datetime.date.today().strftime('%Y-%m-%d')
-FILE_BASE_NAME = '{}-congressperson-civil-names.xz'.format(DATE)
-URL_SCRAPING = 'http://www.camara.leg.br/Internet/deputado/Dep_Detalhe.asp?id={}'
+class CivilNames:
 
-CSV_PARAMS = {
-    'compression': 'xz',
-    'encoding': 'utf-8',
-    'index': False
-}
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DATA_PATH = os.path.join(BASE_DIR, 'data')
+    DATE = datetime.date.today().strftime('%Y-%m-%d')
+    FILE_BASE_NAME = '{}-congressperson-civil-names.xz'.format(DATE)
+    URL_SCRAPING = 'http://www.camara.leg.br/Internet/deputado/Dep_Detalhe.asp?id={}'
 
+    CSV_PARAMS = {
+        'compression': 'xz',
+        'encoding': 'utf-8',
+        'index': False
+    }
 
-def find_newest_file(name):
+    def __init__(self):
+        self.total = 0
 
-    date_regex = re.compile('\d{4}-\d{2}-\d{2}')
+    def find_newest_file(self, name):
 
-    matches = (date_regex.findall(f) for f in os.listdir(DATA_PATH))
-    dates = sorted(set([l[0] for l in matches if l]), reverse=True)
+        date_regex = re.compile('\d{4}-\d{2}-\d{2}')
 
-    for date in dates:
-        filename = os.path.join(DATA_PATH, '{}-{}.xz'.format(date, name))
+        matches = (date_regex.findall(f) for f in os.listdir(self.DATA_PATH))
+        dates = sorted(set([l[0] for l in matches if l]), reverse=True)
 
-        if os.path.isfile(filename):
-            return filename
+        for date in dates:
+            filename = os.path.join(self.DATA_PATH, '{}-{}.xz'.format(date, name))
 
-    return None
+            if os.path.isfile(filename):
+                return filename
 
+        return None
 
-def read_csv(name):
+    def read_csv(self, name):
 
-    newest_file = find_newest_file(name)
-    if newest_file is None:
-        raise TypeError('Could not find the dataset for {%s}.' % newest_file)
+        newest_file = self.find_newest_file(name)
+        if newest_file is None:
+            raise TypeError('Could not find the dataset for {%s}.' % newest_file)
 
-    return pd.read_csv(newest_file, dtype={
-                                        'congressperson_id': np.str
-                                    })
+        return pd.read_csv(newest_file, dtype={
+                                            'congressperson_id': np.str
+                                        })
 
+    def get_all_congresspeople_ids(self):
+        print('Fetching all congresspeople ids...')
 
-def get_all_congress_people_ids():
-    print('Fetching all congress people ids...')
+        ids = (self.read_csv(name)['congressperson_id'] for name in ['current-year', 'last-year', 'previous-years'])
+        distinct_ids = pd.concat(ids).unique()
+        self.total = len(distinct_ids)
 
-    ids = (read_csv(name)['congressperson_id'] for name in ['current-year', 'last-year', 'previous-years'])
-    return list(str(idx).replace('\n', '').strip() for idx in pd.concat(ids).unique())
+        yield from (str(idx).strip() for idx in distinct_ids)
 
+    def write_civil_file(self, congressperson_civil_names):
+        df = pd.DataFrame(data=congressperson_civil_names)
 
-def write_civil_file(congressperson_civil_names):
-    print('Writing file...')
-    df = pd.DataFrame(data=congressperson_civil_names)
-    df.to_csv(os.path.join(DATA_PATH, FILE_BASE_NAME), **CSV_PARAMS)
+        print('Writing file...')
+        df.to_csv(os.path.join(self.DATA_PATH, self.FILE_BASE_NAME), **self.CSV_PARAMS)
 
-    print('Done.')
+        print('Done.')
 
+    def get_civil_names(self):
 
-def get_civil_names():
+        congresspeople_ids = self.get_all_congresspeople_ids()
+        for ind, congress_id in enumerate(congresspeople_ids):
 
-    congress_people_ids = get_all_congress_people_ids()
-    total = len(congress_people_ids)
-    congress_civil_name = []
+            page = requests.get(self.URL_SCRAPING.format(congress_id))
+            data = str(page.content.decode('utf-8'))
 
-    for ind, congress_id in enumerate(congress_people_ids):
+            if page.status_code != 200:
+                msg = 'HTTP request to {} failed with status code {}'
+                print(msg.format(self.URL_SCRAPING.format(congress_id), page.status_code))
 
-        page = requests.get(URL_SCRAPING.format(congress_id))
-        data = str(page.content.decode('utf-8'))
+            try:
+                soup = BeautifulSoup(data, 'html.parser')
+                attributes = soup.findAll('ul', attrs={'class': 'visualNoMarker'})[0]
+                line_name = attributes.find('li')
 
-        if page.status_code != 200:
-            raise TypeError('could not request')
+                # extract tag strong from line_name
+                [x.extract() for x in line_name('strong')]
 
-        try:
-            parliamentary = {}
+                print('Processed {} out of {} ({:.2f}%)'.format(ind, self.total, ind / self.total * 100))
 
-            soup = BeautifulSoup(data, 'html.parser')
-            attributes = soup.findAll('ul', attrs={'class': 'visualNoMarker'})[0]
-            line_name = attributes.find('li')
-
-            # extract tag strong from line_name
-            [x.extract() for x in line_name('strong')]
-
-            parliamentary['congressperson_id'] = congress_id
-            parliamentary['civil_name'] = line_name.text.strip()
-
-            congress_civil_name.append(parliamentary)
-
-            print('Processed {} out of {} ({:.2f}%)'.format(ind, total, ind / total * 100))
-        except IndexError:
-            print('Could not parse data')
-
-    write_civil_file(congress_civil_name)
+                yield dict(congressperson_id=congress_id, civil_name=line_name.text.strip())
+            except IndexError:
+                print('Could not parse data')
 
 if __name__ == '__main__':
-    get_civil_names()
+    civil_names = CivilNames()
+    civil_names.write_civil_file(civil_names.get_civil_names())
+
