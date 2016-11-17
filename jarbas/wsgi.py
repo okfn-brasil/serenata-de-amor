23cc3552eb1ae77705cfd76e9d88ccd1d4213c58
Eduardo Cuducos <cuducos@gmail.com>
2016-11-16 22:57:22 -0200

    Temporaly serve static files from Django

--- jarbas/wsgi.py
+++ jarbas/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jarbas.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
