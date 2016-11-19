d0d48923b9db0b68647d7fb05495aa5178c00e82
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2016-11-19 19:23:17 -0200

    Add conclusions to doublechecking-net-values analysis

--- develop/2016-11-13-irio-doublechecking-net-values.py
+++ develop/2016-11-13-irio-doublechecking-net-values.py
@@ -52,9 +52,26 @@ dataset[dataset['document_value'].isnull()]
 dataset[dataset['document_value'].isnull()].iloc[0]
 
 
+# In[7]:
+
+is_reimbursed_but_has_no_value = dataset['reimbursement_number'].notnull() &     dataset['document_value'].isnull()
+docs = dataset.loc[is_reimbursed_but_has_no_value, ['document_id', 'document_value', 'remark_value', 'net_value']]
+docs
+
+
+# None of the these documents have digitalized receipts.
+
+# In[8]:
+
+def jarbas_url(document_id):
+    return 'http://jarbas.datasciencebr.com/#/document_id/{}'.format(document_id)
+
+docs['document_id'].apply(jarbas_url).tolist()
+
+
 # Since we expect precision in our `net_value` calculation, I'm going to use integers and not floats.
 
-# In[7]:
+# In[9]:
 
 import math
 
@@ -65,17 +82,17 @@ dataset['net_value_int'] = (dataset['net_value'] * 100.).apply(math.ceil).astype
 dataset['calc_net_value_int'] = dataset['document_value_int'] - dataset['remark_value_int']
 
 
-# In[8]:
+# In[10]:
 
 ((dataset['calc_net_value_int'] - dataset['net_value_int']) != 0).sum()
 
 
-# In[9]:
+# In[11]:
 
 dataset.iloc[0]
 
 
-# In[10]:
+# In[12]:
 
 dataset['diff_net_value'] = dataset['net_value_int'] - dataset['calc_net_value_int']
 dataset.loc[dataset['diff_net_value'] != 0, 'diff_net_value'].describe()
@@ -83,25 +100,25 @@ dataset.loc[dataset['diff_net_value'] != 0, 'diff_net_value'].describe()
 
 # What's the number of records with distinct values of `net_value` and our own `net_value`, considering acceptable a maximum difference of 2 cents?
 
-# In[11]:
+# In[13]:
 
 with_significant_difference = dataset.loc[dataset['diff_net_value'].abs() > 2]
 
 
-# In[12]:
+# In[14]:
 
 with_significant_difference['subquota_description'].describe()
 
 
 # It's not just a single subquota, but almost all of them. Probably means that there's something intrinsic in the dataset we still don't know. 
 
-# In[13]:
+# In[15]:
 
 print(len(dataset['subquota_description'].unique()))
 print(len(with_significant_difference['subquota_description'].unique()))
 
 
-# In[14]:
+# In[16]:
 
 from altair import *
 
@@ -114,14 +131,14 @@ Chart(with_significant_difference).mark_bar().encode(
 )
 
 
-# In[15]:
+# In[17]:
 
 with_significant_difference['subquota_description'].unique()
 
 
 # I want to perform a further investigation in flight ticket issues, since [it's already documented](http://jarbas.datasciencebr.com/static/ceap-datasets.html) that `net_value`'s may be negative given canceled flights.
 
-# In[16]:
+# In[18]:
 
 flight_ticket_issues = with_significant_difference['subquota_description'] == 'Flight ticket issue'
 with_significant_difference.loc[flight_ticket_issues, 'net_value'].head(10)
@@ -129,67 +146,67 @@ with_significant_difference.loc[flight_ticket_issues, 'net_value'].head(10)
 
 # Here we have 3 documents with `reimbursement_number` equal to zero. How about disconsidering them to start? If there's no `reimbursement_number`, we could assume that no reimbursement happened (if the data says data, the Chamber of Deputies should be contacted to confirm the affirmation).
 
-# In[17]:
+# In[19]:
 
 with_significant_difference.loc[flight_ticket_issues &                                 (with_significant_difference['congressperson_id'] == '178983')]
 
 
-# In[18]:
+# In[20]:
 
 dataset['reimbursement_number'] =     dataset['reimbursement_number'].replace('0', None)
 dataset = dataset.dropna(subset=['reimbursement_number'])
 
 
-# In[19]:
+# In[21]:
 
 dataset['document_id'].isnull().sum()
 
 
-# In[20]:
+# In[22]:
 
 with_significant_difference = dataset.loc[dataset['diff_net_value'].abs() > 2]
 len(with_significant_difference)
 
 
-# In[21]:
+# In[23]:
 
 flight_ticket_issues = with_significant_difference['subquota_description'] == 'Flight ticket issue'
 with_significant_difference.loc[flight_ticket_issues].head()
 
 
-# In[22]:
+# In[24]:
 
 with_significant_difference.loc[flight_ticket_issues].iloc[0]
 
 
-# In[23]:
+# In[25]:
 
 dataset[(dataset['congressperson_name'] == 'CLARISSA GAROTINHO') &         (dataset['month'] == 4) &         (dataset['year'] == 2016) &         (dataset['subquota_description'] == 'Flight ticket issue')]['net_value_int'].sum()
 
 
-# In[24]:
+# In[26]:
 
 dataset.loc[dataset['document_number'] == 'Bilhete: VHVK6G']
 
 
-# In[25]:
+# In[27]:
 
 dataset.loc[dataset['document_number'] == 'Bilhete: VHVK6G'].iloc[0]
 
 
-# In[26]:
+# In[28]:
 
 dataset.loc[dataset['document_number'] == 'Bilhete: VHVK6G'].iloc[1]
 
 
-# In[27]:
+# In[29]:
 
 dataset.loc[dataset['document_number'] == 'Bilhete: VHVK6G'].iloc[2]
 
 
 # The dataset contains multiple records for the same `document_number`. In this case, they correspond to multiple flight tickets to the same flight, but for distinct passengers.
 
-# In[28]:
+# In[30]:
 
 dataset.loc[dataset['document_number'] == 'Bilhete: VHVK6G',
             ['document_value_int', 'remark_value_int', 'document_number', 'reimbursement_number', 'passenger', 'net_value_int', 'calc_net_value_int']]
@@ -201,7 +218,7 @@ dataset.loc[dataset['document_number'] == 'Bilhete: VHVK6G',
 # 
 # Together, `document_value`, `remark_value` and `net_value` make sense.
 
-# In[29]:
+# In[31]:
 
 with_significant_difference[with_significant_difference['document_id'] == '5914504']
 
@@ -210,32 +227,32 @@ with_significant_difference[with_significant_difference['document_id'] == '59145
 # 
 # Let's prove the affirmation above analysing the whole dataset and checking if the values make more sense after.
 
-# In[30]:
+# In[32]:
 
 data_with_id = dataset[(~dataset['document_id'].isnull()) &
                        (~dataset['year'].isnull()) &
                        (~dataset['applicant_id'].isnull())]
 
 
-# In[31]:
+# In[33]:
 
 keys = ['applicant_id', 'year', 'document_id']
 grouped = data_with_id.groupby(keys)
 len(grouped)
 
 
-# In[32]:
+# In[34]:
 
 reimbursement_numbers = grouped['reimbursement_number'].agg(lambda x: ','.join(set(x))).reset_index()
 agg_net_values_int = grouped['net_value_int'].agg(np.sum).reset_index()
 
 
-# In[33]:
+# In[35]:
 
 agg_net_values_int.head()
 
 
-# In[34]:
+# In[36]:
 
 agg_data = pd.merge(pd.merge(reimbursement_numbers, agg_net_values_int, on=keys),
                     data_with_id,
@@ -246,19 +263,19 @@ agg_data.head()
 
 # There are 2,072,559 documents in the datasets, but when considering just the combination of non-empty values for `applicant_id`, `year` and `document_id`, half of that is found. In other words, the CEAP seem to have paid for about 1MM expenses so far.
 
-# In[35]:
+# In[37]:
 
 len(agg_data)
 
 
-# In[36]:
+# In[38]:
 
 agg_data.drop_duplicates(subset=keys, inplace=True)
 
 len(agg_data)
 
 
-# In[37]:
+# In[39]:
 
 agg_data.drop(['reimbursement_number_from_original',
                'net_value_int_from_original'],
@@ -266,7 +283,7 @@ agg_data.drop(['reimbursement_number_from_original',
               inplace=True)
 
 
-# In[38]:
+# In[40]:
 
 agg_data['document_value_int'] = (agg_data['document_value'] * 100.).apply(math.ceil).astype(np.int)
 agg_data['remark_value_int'] = (agg_data['remark_value'] * 100.).apply(math.ceil).astype(np.int)
@@ -275,21 +292,21 @@ agg_data['calc_net_value_int'] = agg_data['document_value_int'] - agg_data['rema
 agg_data['diff_net_value'] = agg_data['net_value_int'] - agg_data['calc_net_value_int']
 
 
-# In[39]:
+# In[41]:
 
 with_significant_difference = agg_data.loc[agg_data['diff_net_value'].abs() > 2]
 
 
 # Disconsidering multiple records with this same combination, just 744 remain with a large difference between our own calculation of `net_value`s and the value directly in the original dataset.
 
-# In[40]:
+# In[42]:
 
 len(with_significant_difference)
 
 
 # The majority of records were explained by the previous test, having all the flight ticket issues disappeared (though a few "Flight tickets" remain).
 
-# In[41]:
+# In[43]:
 
 Chart(with_significant_difference).mark_bar().encode(
     x=X('subquota_description:O',
@@ -300,33 +317,33 @@ Chart(with_significant_difference).mark_bar().encode(
 )
 
 
-# In[42]:
+# In[44]:
 
 agg_data[agg_data['reimbursement_number'].str.contains(',')]
 
 
 # The document with major positive difference in the dataset doesn't seem to have any irregularity. The deputy asked the reimbursement for R\$ 25.000,00, but received just R\$ 6.376,11.
 
-# In[43]:
+# In[45]:
 
 with_significant_difference.sort_values('diff_net_value').iloc[0]
 
 
 # Deputies receiving less money than they asked is OK. What if they are receiving more than they should (values in the `document_value` column)?
 
-# In[44]:
+# In[46]:
 
 has_extra_reimbursement = with_significant_difference['diff_net_value'] > 0
 extra_reimbursement = with_significant_difference[has_extra_reimbursement].     sort_values('diff_net_value', ascending=False)
 len(extra_reimbursement)
 
 
-# In[45]:
+# In[47]:
 
 extra_reimbursement.head()
 
 
-# In[46]:
+# In[48]:
 
 Chart(extra_reimbursement).mark_bar().encode(
     x=X('subquota_description:O',
@@ -337,29 +354,29 @@ Chart(extra_reimbursement).mark_bar().encode(
 )
 
 
-# In[47]:
+# In[49]:
 
 extra_reimbursement.sort_values('diff_net_value', ascending=False).iloc[0]
 
 
 # This document says that a deputy received R\$ 3.226,59 more than it should. Let's check all the datasets we have to see if there's any mistake in the calculation.
 
-# In[48]:
+# In[50]:
 
 dataset[dataset['document_id'] == '5951638']
 
 
-# In[49]:
+# In[51]:
 
 data_with_id[data_with_id['document_id'] == '5951638']
 
 
-# In[50]:
+# In[52]:
 
 extra_reimbursement[extra_reimbursement['document_id'] == '5951638']
 
 
-# In[51]:
+# In[53]:
 
 extra_reimbursement[extra_reimbursement['document_id'] == '5951638'].iloc[0]
 
@@ -368,36 +385,46 @@ extra_reimbursement[extra_reimbursement['document_id'] == '5951638'].iloc[0]
 # 
 # There's a possibility that this extra payment counted towards other requests for reimbursements. Let's check.
 
-# In[52]:
+# In[54]:
 
 dataset[(dataset['applicant_id'] == 2899) &         (dataset['subquota_number'] == 3) &         (dataset['year'] == 2016) &         (dataset['month'].isin([1, 2, 3]))]
 
 
 # Can't see anything proving that. Going to analize the next reimbursement.
 
-# In[53]:
+# In[55]:
 
 extra_reimbursement.sort_values('diff_net_value', ascending=False).iloc[1]
 
 
-# In[54]:
+# In[56]:
 
 (extra_reimbursement['remark_value'] > 0).sum()
 
 
 # Nope. Not a clue.
 
-# In[55]:
+# In[57]:
 
 extra_reimbursement.sort_values('diff_net_value', ascending=False).iloc[2]
 
 
 # Not yet.
 
-# In[56]:
+# In[58]:
 
 extra_reimbursement.sort_values('diff_net_value', ascending=False).iloc[3]
 
+
+# ## Conclusions
+# 
+# **There are documents (aka rows in the CEAP dataset) without a corresponding reimbursement number.** They don't even appear in the Chamber of Deputies website, but just in the XML provided by them. These records are identifiable by lack of a value for `reimbursement_number`. The reason why they were not reimbursement? Still not answered by the Chamber of Deputies.
+# 
+# **A request for reimbursement may be fulfilled in multiple reimbursements.** When this happens, multiple rows in the dataset, summing their `net_value`s will give `document_value - remark_value` as expected.
+# 
+# **There are 17 documents theorically reimbursed, but without `document_value` and a digitalized receipt.** Couldn't give further explanation without Chamber of Deputies' help.
+# 
+# **55 documents were theorically reimbursed in a value larger than they should.** Again, even a manual investigation couldn't explain their existence.
 
 # In[ ]:
 
