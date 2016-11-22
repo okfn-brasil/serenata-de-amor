6a837feced2d0ab0fe08cd56a3326c56d2a0ef16
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-11-22 09:54:00 -0300

    Fix tests

--- jarbas/frontend/tests/elm-tests/Tests.elm
+++ jarbas/frontend/tests/elm-tests/Tests.elm
@@ -2,8 +2,7 @@ module Tests exposing (..)
 
 import Test exposing (..)
 import Expect
-import String
-import Documents exposing (..)
+import Documents.Update exposing (..)
 
 
 all : Test
@@ -11,8 +10,8 @@ all =
     describe "Documents"
         [ test "Documents.totalPages" <|
             \() ->
-                Expect.equal (Documents.totalPages 20) 3
+                Expect.equal (Documents.Update.totalPages 20) 3
         , test "Documents.onlyDigits" <|
             \() ->
-                Expect.equal (Documents.onlyDigits "12a3") "123"
+                Expect.equal (Documents.Update.onlyDigits "12a3") "123"
         ]
