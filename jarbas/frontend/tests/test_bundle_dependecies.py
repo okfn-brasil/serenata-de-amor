26581b24dd00c3b0a0928fe0b24ae129c701fb58
Eduardo Cuducos <cuducos@gmail.com>
2016-11-23 19:36:36 -0200

    Fix test for Elm files lookup

--- jarbas/frontend/tests/test_bundle_dependecies.py
+++ jarbas/frontend/tests/test_bundle_dependecies.py
@@ -1,3 +1,4 @@
+from glob import glob
 from django.test import TestCase
 from webassets.bundle import get_all_bundle_files
 
@@ -7,5 +8,6 @@ from jarbas.frontend.assets import elm
 class TestDependencies(TestCase):
 
     def test_dependencies(self):
+        expected = len(glob('jarbas/frontend/elm/**/*.elm', recursive=True))
         files = set(get_all_bundle_files(elm))
-        self.assertEqual(9, len(files), files)
+        self.assertEqual(expected, len(files), files)
