3c626088a7174c28d1b1a093143c4ca7b2b154fc
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2016-11-23 18:30:42 -0200

    Make script to collect data from Yelp save to a CSV file

--- src/fetch_yelp_info.py
+++ src/fetch_yelp_info.py
@@ -9,9 +9,9 @@ import configparser
 import pandas as pd
 import numpy as np
 
-REIMBURSEMENTS_DATASET_PATH = os.path.join('data', '2016-11-22-reimbursements.xz')
+REIMBURSEMENTS_DATASET_PATH = os.path.join('data', '2016-11-19-reimbursements.xz')
 COMPANIES_DATASET_PATH = os.path.join('data', '2016-09-03-companies.xz')
-YELP_DATASET_PATH = os.path.join('data', 'yelp-companies.json')
+YELP_DATASET_PATH = os.path.join('data', 'yelp-companies.xz')
 
 def companies():
   # Loading reimbursements
@@ -26,6 +26,7 @@ def companies():
   all_companies = pd.read_csv(COMPANIES_DATASET_PATH,
                               low_memory=False,
                               dtype={'trade_name': np.str})
+  all_companies = all_companies[all_companies['trade_name'].notnull()]
   # Cleaning up companies CNPJs
   all_companies['clean_cnpj'] = all_companies['cnpj'].map(cleanup_cnpj)
   # Filtering only companies that are in meal reimbursements
@@ -36,20 +37,21 @@ def cleanup_cnpj(cnpj):
   digits = re.findall(regex, '%s' % cnpj)
   return ''.join(digits)
 
-def remaining_companies(companies):
-  df = load_companies_dataset()
-  return companies[~companies['cnpj'].isin(df['cnpj'])]
+def remaining_companies(fetched_companies, companies):
+  return companies[~companies['cnpj'].isin(fetched_companies['cnpj'])]
 
 def load_companies_dataset():
   if os.path.exists(YELP_DATASET_PATH):
-      return pd.read_json(YELP_DATASET_PATH)
+      return pd.read_csv(YELP_DATASET_PATH)
   else:
       return pd.DataFrame(columns=['cnpj'])
 
 def parse_fetch_info(response):
   if response.status_code == 200:
     json = response.json()
-    return json['businesses'][0]
+    results = json['businesses']
+    if results:
+        return results[0]
 
 # ----------------------------
 # Request to yelp API getting by term and zip code
@@ -70,21 +72,19 @@ companies_w_meal_expense = companies()
 companies_trade_names = companies_w_meal_expense.trade_name.dropna()
 companies_zip_code = companies_w_meal_expense.zip_code.dropna()
 
-fetched_companies = []
+fetched_companies = load_companies_dataset()
+companies_to_fetch = remaining_companies(fetched_companies, companies())[:20]
 
-for _, company in remaining_companies(companies()).iterrows():
+for _, company in companies_to_fetch.iterrows():
     print('Fetching %s - CNPJ: %s' % (company['trade_name'], company['zip_code']))
     fetched_company = fetch_yelp_info(company['trade_name'], company['zip_code'])
     if fetched_company:
-        print('Successifuly matched %s' % fetched_company['name'])
+        print('Successfuly matched %s' % fetched_company['name'])
         fetched_company['scraped_at'] = datetime.datetime.utcnow().isoformat()
         fetched_company['cnpj'] = company['cnpj']
-        fetched_companies.append(fetched_company)
+        fetched_companies = fetched_companies.append(pd.Series(fetched_company),
+                                                     ignore_index=True)
     else:
         print('Not found')
-if fetched_companies:
-    print('Saving it all!')
-    with open(FOURSQUARE_DATASET_PATH, 'w') as outfile:
-        json.dump(fetched_companies, outfile)
-else:
-    print('Nothing to save.')
+
+fetched_companies.to_csv(YELP_DATASET_PATH, compression='xz', index=False)
