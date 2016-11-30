7f97c7c54e3f3fe8899651788f3cb12d1deeaf8a
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2016-11-30 14:19:16 -0200

    Fix test

--- test/test_traveled_speeds.py
+++ test/test_traveled_speeds.py
@@ -8,8 +8,8 @@ class TestTraveledSpeeds(unittest.TestCase):
     def test_it_works(self):
         self.assertEqual(4, self.result)
 
-    def test_it_fails(self):
-        self.assertEqual(5, self.result)
+    def test_it_works2(self):
+        self.assertNotEqual(5, self.result)
 
 
 
