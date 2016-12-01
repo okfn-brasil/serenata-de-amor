2c50795b40425430c18de52900bdf9995580a4be
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2016-11-30 22:27:06 -0200

    Implement TraveledSpeedsClassifier

--- rosie/traveled_speeds_classifier.py
+++ rosie/traveled_speeds_classifier.py
@@ -1,7 +1,76 @@
 from datetime import datetime
+from itertools import combinations
+import os.path
+from geopy.distance import vincenty as distance
 import numpy as np
 import pandas as pd
 from sklearn.base import TransformerMixin
+from sklearn.utils.validation import check_is_fitted
+
 
 class TraveledSpeedsClassifier(TransformerMixin):
-    pass
+
+    AGG_KEYS = ['applicant_id', 'issue_date']
+
+    def __init__(self, contamination=.001):
+        if contamination in [0, 1]:
+            raise ValueError('contamination must be greater than 0 and less than 1')
+
+        self.contamination = contamination
+
+    def fit(self, X):
+        self._X = self.__aggregate_dataset(X)
+        self.polynomial = np.polyfit(self._X['expenses'].astype(np.long),
+                                     self._X['distance_traveled'].astype(np.long),
+                                     3)
+        self._polynomial_fn = np.poly1d(self.polynomial)
+        self._X = self.__classify_dataset(self._X)
+        return self
+
+    def predict(self, X):
+        check_is_fitted(self, 'polynomial')
+
+        _X = X.copy()
+        _X = self.__aggregate_dataset(_X)
+        _X = self.__classify_dataset(_X)
+        _X = pd.merge(X, _X, how='left', left_on=self.AGG_KEYS, right_on=self.AGG_KEYS)
+        is_outlier = \
+            (_X['subquota_description'] == 'Congressperson meal') & \
+            (_X['expenses_threshold_outlier'] | \
+             _X['traveled_speed_outlier'])
+        y = is_outlier.astype(np.int).replace({1: -1, 0: 1})
+        return y
+
+    def __aggregate_dataset(self, X):
+        _X = X.query("subquota_description == 'Congressperson meal'")
+        _X = _X.groupby(self.AGG_KEYS) \
+            .apply(self.__calculate_sum_distances).reset_index() \
+            .rename(columns={0: 'distance_traveled'})
+        expenses = X.groupby(self.AGG_KEYS)['applicant_id'] \
+            .agg({'expenses': len}).reset_index()
+        _X = pd.merge(_X, expenses, left_on=self.AGG_KEYS, right_on=self.AGG_KEYS)
+        return _X
+
+    def __classify_dataset(self, X):
+        _X = X.copy()
+        _X['expected_distance'] = self._polynomial_fn(_X['expenses'])
+        _X['diff_distance'] = abs(_X['expected_distance'] - _X['distance_traveled'])
+        _X['expenses_threshold_outlier'] = _X['expenses'] > 8
+        threshold = self.__threshold_for_contamination(_X, self.contamination)
+        _X['traveled_speed_outlier'] = _X['diff_distance'] > threshold
+        return _X
+
+    def __calculate_sum_distances(self, X):
+        coordinate_list = X[['latitude', 'longitude']].values
+        edges = list(combinations(coordinate_list, 2))
+        return np.sum([distance(edge[0][1:], edge[1][1:]).km for edge in edges])
+
+    def __threshold_for_contamination(self, X, expected_contamination):
+        possible_thresholds = range(1, int(X['expected_distance'].max()), 50)
+        results = [(self.__contamination(X, t), t) for t in possible_thresholds]
+        best_choice = min(results, key=lambda x: abs(x[0] - expected_contamination))
+        return best_choice[1]
+
+    def __contamination(self, X, threshold):
+        return (X['diff_distance'] > threshold).sum() / \
+            (len(X) - X['expenses_threshold_outlier'].sum())
--- test/test_traveled_speeds_classifier.py
+++ test/test_traveled_speeds_classifier.py
@@ -1,39 +1,63 @@
-from unittest import TestCase, main, skip
+from unittest import TestCase, main
 from rosie.traveled_speeds_classifier import TraveledSpeedsClassifier
+import numpy as np
+from numpy.testing import assert_array_equal
+import pandas as pd
+import sklearn
+
 
 class TestTraveledSpeedsClassifier(TestCase):
 
     def setUp(self):
+        self.dataset = pd.read_csv('test/reimbursements_and_companies.csv',
+                                   index_col='id',
+                                   dtype={'cnpj_cpf': np.str})
         self.subject = TraveledSpeedsClassifier()
+        self.subject.fit(self.dataset)
 
     def test_it_works(self):
         self.assertEqual(4, 2 + 2)
 
-    @skip('not implemented')
+    def test_fit_learns_a_polynomial_for_regression(self):
+        self.assertIsInstance(self.subject.polynomial, np.ndarray)
+
+    def test_predict_doesnt_work_before_fitting_the_model(self):
+        subject = TraveledSpeedsClassifier()
+        with self.assertRaises(sklearn.exceptions.NotFittedError):
+            subject.predict(self.dataset)
+
     def test_predict_returns_a_prediction_for_each_observation(self):
-        pass
+        prediction = self.subject.predict(self.dataset)
+        self.assertEqual(len(prediction), len(self.dataset))
 
-    @skip('not implemented')
     def test_predict_considers_meal_reimbursements_in_days_with_more_than_8_outliers(self):
-        pass
+        prediction = self.subject.predict(self.dataset)
+        assert_array_equal(np.repeat(-1, 9), prediction[:9])
 
-    @skip('not implemented')
     def test_predict_considers_non_meal_reibursement_an_inlier(self):
-        pass
+        prediction = self.subject.predict(self.dataset)
+        self.assertEqual(1, prediction[12])
 
-    @skip('not implemented')
     def test_predict_considers_non_meal_reibursement_an_inlier_even_when_more_than_8_meal_reimbursements(self):
-        pass
+        prediction = self.subject.predict(self.dataset)
+        self.assertEqual(1, prediction[9])
 
-    @skip('not implemented')
+    def test_predict_uses_learned_thresholds_from_fit_dataset(self):
+        subject = TraveledSpeedsClassifier(contamination=.6)
+        subject.fit(self.dataset)
+        assert_array_equal(
+            np.repeat(-1, 6), subject.predict(self.dataset[11:17]))
+
     def test_predict_limits_the_number_of_outliers_with_contamination_param(self):
-        pass
+        subject = TraveledSpeedsClassifier(contamination=.6)
+        subject.fit(self.dataset)
+        self.assertEqual(15, (subject.predict(self.dataset) == -1).sum())
 
-    @skip('not implemented')
     def test_predict_validates_range_of_values_for_contamination_param(self):
-        pass
-
-
+        with self.assertRaises(ValueError):
+            TraveledSpeedsClassifier(contamination=0)
+        with self.assertRaises(ValueError):
+            TraveledSpeedsClassifier(contamination=1)
 
 
 if __name__ == '__main__':
