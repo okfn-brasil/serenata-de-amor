e5608af808dadbc001f1eb7bb1992bc5ece70508
Samuel Grigolato <samuel.grigolato@gmail.com>
2016-12-02 11:16:56 -0200

    Rename notebooks in order to adhere to new rules

--- develop/2016-08-08-samuelgrigolato-lodging-expenses.py
+++ develop/2016-08-08-samuelgrigolato-lodging-expenses.py
@@ -18,7 +18,7 @@ import pandas as pd
 import numpy as np
 
 
-# In[11]:
+# In[2]:
 
 data = pd.read_csv('../data/2016-11-19-reimbursements.xz',
                   dtype={ 'cnpj_cpf': np.str, 'reimbursement_numbers': np.str })
@@ -26,7 +26,7 @@ data = pd.read_csv('../data/2016-11-19-reimbursements.xz',
 
 # First thing we should do is to filter our dataset according to @Irio's suggestion.
 
-# In[13]:
+# In[3]:
 
 filtered_data = data[data['subquota_description'] == 'Lodging, except for congressperson from Distrito Federal']
 filtered_data.head(2)
@@ -34,7 +34,7 @@ filtered_data.head(2)
 
 # Next, it is handy to further simplify our model. Lets focus only on the hotel's social ID (CNPJ) and the receipt's declared value.
 
-# In[16]:
+# In[4]:
 
 lodging_data = filtered_data[['cnpj_cpf', 'total_net_value']]
 lodging_data.head()
@@ -42,7 +42,7 @@ lodging_data.head()
 
 # Now let's find out the average value and standard deviation for each supplier that has at least 10 receipts (so our results have a higher chance of being meaningful).
 
-# In[17]:
+# In[5]:
 
 per_supplier_data = lodging_data.groupby('cnpj_cpf').agg({ 'total_net_value': ['count', 'mean', 'std'] })
 meaningful_supplier_data = per_supplier_data[per_supplier_data['total_net_value']['count'] >= 10]
@@ -55,7 +55,7 @@ meaningful_supplier_data.head()
 
 # With this data we can join back with our original dataset and find potentially suspicious receipts.
 
-# In[29]:
+# In[6]:
 
 joined_data = pd.merge(filtered_data, meaningful_supplier_data, left_on='cnpj_cpf', right_index=True)
 suspicous_predicate = joined_data.total_net_value > (joined_data['total_net_value mean'] + (joined_data['total_net_value std'] * 2))
