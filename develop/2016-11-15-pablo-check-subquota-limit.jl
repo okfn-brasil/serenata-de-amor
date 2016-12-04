6e4fe79ebe1ca3619f8256c604349ae05120f8a3
pmargreff <pmargreff@gmail.com>
2016-12-03 23:40:09 -0200

    fix: run the analysis again for the fixed net_values

--- develop/2016-11-15-pablo-check-subquota-limit.jl
+++ develop/2016-11-15-pablo-check-subquota-limit.jl
@@ -1,17 +1,11 @@
 
-using DataFrames, Gadfly
+using DataFrames, PlotlyJS
 
 limit = readtable("../data/2016-11-15-limit-by-subquota.csv")
 
-limit[:diff] = map((x,y) -> x-y , limit[:value], limit[:max])
+limit[:diff] = map((x,y) -> round(x-y,2) , limit[:value], limit[:max])
+data = bar(;x=limit[:name],y=limit[:diff])
 
+plot(data)
 
-by(limit, [:subquota_description], limit -> sum(limit[:diff]))
-
-plot(limit,ygroup="subquota_description" ,y="diff", Geom.subplot_grid(Geom.point))
-
-plot(limit, x="diff", color="subquota_description", Geom.density)
-
-
-set_default_plot_size(22cm, 28cm)
-plot(limit, ygroup="subquota_description", x="month",  y="diff", color="subquota_description", Geom.subplot_grid(Geom.point))
+sum(limit[:diff])
--- src/get_by_subquota_limit.jl
+++ src/get_by_subquota_limit.jl
@@ -1,58 +1,62 @@
 using DataFrames
 
 # join the files
-files = ["./data/2016-08-08-previous-years.csv",
-"./data/2016-08-08-last-year.csv",
-"./data/2016-08-08-current-year.csv"]
+file = "./data/2016-11-19-reimbursements.csv"
 
-df = readtable(files[1])
-
-for i in 2:length(files)
-  concatdf = readtable(files[i])
-  df = vcat(df, concatdf)
-end
+df = readtable(file)
 
 # group files by [:year, :month, :subquota_description, :congressperson_name]
-dfgroup = by(df, [:year, :month, :subquota_description, :congressperson_name], df -> sum(df[:net_value]))
+dfgroup = by(df, [:year, :month, :subquota_description, :congressperson_name], df -> round(sum(df[:net_values]),2))
 
 nrows, ncolumns = size(dfgroup)
 
-rename!(dfgroup, :x1, :net_value) 
+rename!(dfgroup, :x1, :total_value) 
 
 #create a new df with all data pass the limit
 limit = DataFrame(month = 0, year = 0, name = "", subquota_description = "",value = 0.0, max = 0.0)
 
 for row in eachrow(dfgroup)
-  if (row[:year] >= 2013 && row[:month] >10) && !(row[:year] >= 2015 && row[:month] >2)
-    if row[:subquota_description] == "Automotive vehicle renting or charter" && row[:net_value] > 10001
-      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:net_value], 10000]))
+  if (row[:year] > 2013 || row[:year] == 2013 && row[:month] > 10) &&
+    !(row[:year] > 2015 || row[:year] == 2015 && row[:month] > 2)
+    
+    if row[:subquota_description] == "Automotive vehicle renting or charter" && row[:total_value] > 10001
+      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:total_value], 10000]))
     end
-    if row[:subquota_description] == "Taxi, toll and parking" && row[:net_value] > 2501
-      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:net_value], 2500]))
+    
+    if row[:subquota_description] == "Taxi, toll and parking" && row[:total_value] > 2501
+      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:total_value], 2500]))
     end
-  elseif (row[:year] >= 2014 && row[:month] > 4) && !(row[:year] >= 2015 && row[:month] > 2)
-    if row[:subquota_description] == "Security service provided by specialized company" && row[:net_value] > 8001
-      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:net_value], 8000]))
+  end  
+  
+  if (row[:year] > 2014 || row[:year] == 2014 && row[:month] > 4) &&
+    !(row[:year] > 2015|| row[:year] == 2015 && row[:month] > 2)
+    if row[:subquota_description] == "Security service provided by specialized company" && row[:total_value] > 8001
+      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:total_value], 8000]))
     end
-  elseif row[:year] >= 2015 && row[:month] > 2 
-    if row[:subquota_description] == "Automotive vehicle renting or charter" && row[:net_value] > 10901
-      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:net_value], 10900]))
+  end
+  
+  if (row[:year] > 2015 || row[:year] == 2015 && row[:month] > 2) 
+    if row[:subquota_description] == "Automotive vehicle renting or charter" && row[:total_value] > 10901
+      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:total_value], 10900]))
     end
-    if row[:subquota_description] == "Taxi, toll and parking" && row[:net_value] > 2701
-      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:net_value], 2700]))
+    if row[:subquota_description] == "Taxi, toll and parking" && row[:total_value] > 2701
+      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:total_value], 2700]))
     end
-    if row[:subquota_description] == "Security service provided by specialized company" && row[:net_value] > 8701
-      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:net_value], 8700]))
+    if row[:subquota_description] == "Security service provided by specialized company" && row[:total_value] > 8701
+      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:total_value], 8700]))
     end
-    if row[:subquota_description] == "Participation in course, talk or similar event" && row[:net_value] > 7698
-      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:net_value], 7697]))
+    if row[:subquota_description] == "Participation in course, talk or similar event" && row[:total_value] > 7698
+      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:total_value], 7697]))
     end
-    if row[:subquota_description] == "Fuels and lubricants" && row[:net_value] > 4901 && !(row[:year] >= 2015 && row[:month] > 9)
-      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:net_value], 4900]))
+    if (row[:subquota_description] == "Fuels and lubricants" && row[:total_value] > 4901) &&
+      !(row[:year] > 2015 || row[:year] == 2015 && row[:month] >= 9)
+      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:total_value], 4900]))
     end
-  elseif row[:year] >= 2015 && row[:month] > 9 
-    if row[:subquota_description] == "Fuels and lubricants" && row[:net_value] > 6001
-      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:net_value], 6000]))
+  end
+  
+  if (row[:year] > 2015 || row[:year] == 2015 && row[:month] > 9) 
+    if row[:subquota_description] == "Fuels and lubricants" && row[:total_value] > 6001
+      push!(limit, @data([row[:month], row[:year], row[:congressperson_name], row[:subquota_description] ,row[:total_value], 6000]))
     end
   end
 end
