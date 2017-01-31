592bb625d5fd1ed7bead9adf01f60d107f156ea4
Eduardo Cuducos <cuducos@gmail.com>
2017-01-31 12:10:15 -0200

    Update elm-date-extra and remove locales

--- jarbas/frontend/elm/Format/Date.elm
+++ jarbas/frontend/elm/Format/Date.elm
@@ -2,8 +2,8 @@ module Format.Date exposing (formatDate)
 
 import Date
 import Date.Extra.Config.Config_en_us as Config_en_us
+import Date.Extra.Config.Config_pt_br as Config_pt_br
 import Date.Extra.Format exposing (formatUtc)
-import Format.Date.Config.Config_pt_br as Config_pt_br
 import Internationalization exposing (Language(..))
 
 
