c396b88f4b2252fe73f7a5ff9c22b4ef89499f49
arnaldocorreiafilho <arnaldocorreiadeandradefilho@gmail.com>
2017-02-08 20:04:34 +0000

    file bolsa familia changed

--- src/fetch_family_bag.py
+++ src/fetch_family_bag.py
@@ -8,8 +8,6 @@ import os
 
 count = 0
 for url in url_family_bag.urls():
-    print(url)    
+        wget.download(url,"../data/file"+(str(count)))
     
-    wget.download(url,"../data/file"+(str(count)))
-    count = count + 1
         
