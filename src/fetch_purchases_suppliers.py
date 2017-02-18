aa11139c96e5f5c9337359a19b1a1904a0020f00
Ana Gomes <apgomes88@gmail.com>
2017-02-18 14:59:25 -0200

    Fetch suppliers from compras.dados.gov.br

---
+++ src/fetch_purchases_suppliers.py
@@ -0,0 +1,40 @@
+import requests
+import pandas as pd
+
+
+BASE_DATA_DIR = './data/'
+
+def fetch_suppliers():
+    raw_suppliers = []
+    first_response = requests.get('http://compras.dados.gov.br/fornecedores/v1/fornecedores.json').json()
+    raw_suppliers = raw_suppliers + first_response['_embedded']['fornecedores']
+
+    last_page = first_response['count']
+    for page in range(2, last_page):
+        print('{}/{}'.format(page, last_page))
+        response = requests.get('http://compras.dados.gov.br/fornecedores/v1/fornecedores.json').json()
+        raw_suppliers = raw_suppliers + first_response['_embedded']['fornecedores']
+    return raw_suppliers
+
+
+def prepare_data(raw_data):
+    suppliers_attributes_columns = {'id': 'id', 'cnpj': 'cnpj', 'nome': 'name', 'ativo': 'active',
+    'recadastrado': 'relisted', 'id_municipio': 'city_id', 'uf': 'state', 'id_natureza_juridica': 'legal_nature_id',
+    'id_porte_empresa': 'company_size_id', 'id_ramo_negocio': 'business_id', 'id_unidade_cadastradora': 'responsible_entity',
+    'id_cnae': 'cnae_id', 'habilitado_licitar': 'allowed_to_bid'}
+
+    df = pd.DataFrame(raw_data, columns = suppliers_attributes_columns.keys())
+
+    return df
+
+
+def save_csv(df):
+    df.to_csv(path_or_buf=BASE_DATA_DIR+'purchase_suppliers.xz', sep=',', compression='xz', encoding='utf-8', index=False)
+
+def main():
+    raw_data = fetch_suppliers()
+    suppliers_info = prepare_data(raw_data)
+    save_csv(suppliers_info)
+
+if __name__ == '__main__':
+    main()
