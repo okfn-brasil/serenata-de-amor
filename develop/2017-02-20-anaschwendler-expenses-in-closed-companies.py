3b0e1693a14e200772dd2d88e5b521a4183346f9
Ana Schwendler <schwendler@gmail.com>
2017-02-22 16:01:33 -0300

    Final fixes to notebook

--- develop/2017-02-20-anaschwendler-expenses-in-closed-companies.py
+++ develop/2017-02-20-anaschwendler-expenses-in-closed-companies.py
@@ -13,6 +13,7 @@ from serenata_toolbox.datasets import fetch
 fetch('2016-09-03-companies.xz', '../data')
 fetch('2016-11-19-reimbursements.xz', '../data')
 
+
 # In[2]:
 
 companies = pd.read_csv('../data/2016-09-03-companies.xz', low_memory=False)
@@ -23,6 +24,7 @@ reimbursements = pd.read_csv('../data/2016-11-19-reimbursements.xz',
                              'subquota_number': np.str},
                       low_memory=False)
 
+
 # ## Formatting
 # Formatting companies situation_date and reimbursements issue_date columns to correct date format (will be needed for a query later), and formatting the companies cpnj to a format without dash and dots.
 
@@ -32,11 +34,13 @@ reimbursements['issue_date'] = pd.to_datetime(reimbursements['issue_date'], erro
 companies['situation_date'] = pd.to_datetime(companies['situation_date'], errors='coerce')
 companies['cnpj'] = companies['cnpj'].str.replace(r'\D', '')
 
+
 # In[4]:
 
 statuses = ['BAIXADA', 'NULA', 'SUSPENSA', 'INAPTA']
 not_open = companies[companies['situation'].isin(statuses)]
 not_open[['cnpj', 'situation_date','situation', 'situation_reason']].head(5)
+
 
 # The column situation_date is the one that is interesting. Expenses made after that date should be considered suspicious.
 # 
@@ -47,19 +51,23 @@ not_open[['cnpj', 'situation_date','situation', 'situation_reason']].head(5)
 dataset = pd.merge(reimbursements, not_open, how='inner',
                    left_on='cnpj_cpf', right_on='cnpj')
 
+
 # In[6]:
 
 columns = ['congressperson_name', 'issue_date','cnpj', 'situation_date',
            'situation', 'situation_reason']
 dataset[columns].head(10)
 
+
 # In[7]:
 
 dataset.shape
 
+
 # In[8]:
 
 dataset.iloc[0]
+
 
 # ## Filtering suspicious reimbursements
 # We have all reibursements requested for expenses made in companies that have situation other than "open".
@@ -70,8 +78,10 @@ dataset.iloc[0]
 expenses_in_closed_companies = dataset.query('issue_date > situation_date')
 expenses_in_closed_companies[columns].head()
 
+
 # In[10]:
 
 expenses_in_closed_companies.shape
+
 
 # We can safely say that there are 5222 suspicious reimbursements.
