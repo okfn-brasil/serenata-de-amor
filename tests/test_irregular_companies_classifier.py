331d1b46a8402a54ff5ce9fd93406d23c8b7b4e6
Jessica Temporal <jessicatemporal@gmail.com>
2017-03-08 11:09:01 -0300

    adds fit and transform tests

--- tests/test_irregular_companies_classifier.py
+++ tests/test_irregular_companies_classifier.py
@@ -30,3 +30,10 @@ class TestIrregularCompaniesClassifier(TestCase):
 
     def test_is_valid_reimbursement_based_on_date(self):
         self.assertEqual(self.subject.predict(self.dataset)[5], False)
+
+
+    def test_fit(self):
+        self.assertEqual(self.subject.fit(self.dataset), self.subject)
+
+    def test_transform(self):
+        self.assertEqual(self.subject.transform(), self.subject)
