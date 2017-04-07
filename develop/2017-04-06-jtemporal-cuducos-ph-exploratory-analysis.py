973167dda457a1cb9569582a4a499e9618150f45
Jessica Temporal <jessicatemporal@gmail.com>
2017-04-07 12:06:59 -0300

    adds 'bode assado' topic

--- develop/2017-04-06-jtemporal-cuducos-ph-exploratory-analysis.py
+++ develop/2017-04-06-jtemporal-cuducos-ph-exploratory-analysis.py
@@ -103,28 +103,67 @@ aggregation.head(10)
 
 # Who were the congresspeople that ate more in one day and when?
 
-# In[9]:
+# In[99]:
 
 meals = dataset[dataset['subquota_description'] == 'Congressperson meal']
+meals = meals.reset_index()
 
 
-# In[7]:
+# In[100]:
 
 keys = ['congressperson_name', 'issue_date']
 meals_aggregation = meals.groupby(keys)['total_net_value'].                         agg({'sum': np.sum, 'expenses': len, 'mean': np.mean})
 meals_aggregation['expenses'] = meals_aggregation['expenses'].astype(np.int)
 
 
-# In[8]:
+# In[101]:
 
 meals_aggregation.sort_values(['expenses', 'sum'], ascending=[False, False]).head(20)
 
 
 # What was the highest meal reimbursement, where it was made and by who?
 
-# In[16]:
+# In[102]:
 
 meals[['document_id', 'issue_date', 'total_net_value', 'congressperson_name', 'supplier']].         sort_values('total_net_value', ascending=False).head(10)
+
+
+# ## Bode assado
+
+# In[103]:
+
+from re import compile, search
+
+
+# In[104]:
+
+len(meals)
+
+
+# In[112]:
+
+bode = re.compile('BODE')
+bode_bool = []
+for _ in range(len(meals)):
+    if bode.search(meals.loc[_, 'supplier']):
+        bode_bool.append(True)
+    else:
+        bode_bool.append(False)
+
+
+# In[144]:
+
+bode_meals = meals[bode_bool]
+keys = ['congressperson_id','cnpj_cpf', 'supplier', 'issue_date', 'document_id']
+bode_meals_grouped = bode_meals.groupby(keys)
+bode_meals_grouped.head()
+
+
+# In[145]:
+
+bode_meals_grouped = bode_meals_grouped['total_net_value'].agg({'expense': np.sum}).reset_index()
+bode_meals_grouped = bode_meals_grouped.sort_values('expense', ascending=False)
+bode_meals_grouped.head(20)
 
 
 # In[ ]:
