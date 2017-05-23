f399881e360206699fa9f3657b116968cd24b588
Ana Schwendler <schwendler@gmail.com>
2017-05-23 19:40:35 +0200

    Introduce method to see if there is transparency portal for cities

--- develop/2017-05-19-jtemporal-cities-population-study.py
+++ develop/2017-05-19-jtemporal-cities-population-study.py
@@ -11,9 +11,10 @@
 import pandas as pd
 import numpy as np
 
-
+# We first collected the data with population estimatives, 
+# we can use it later to do some comparisions or to use it later
 cities = pd.read_excel('../data/Cidades - estimativa 2013.xlsx',
-                       dtype={'COD. UF': np.str, 'COD. MUNIC': str},
+                       converters={'COD. UF': np.str, 'COD. MUNIC': np.str},
                        sheetname=None, header=0)
 
 
@@ -113,12 +114,65 @@ brazilian_states = {'RO': 'rondonia',
 census_link = "ftp.ibge.gov.br/Censos/Censo_Demografico_2010/resultados/total_populacao_{}.zip"
 
 
+# In[10]:
+
+from serenata_toolbox.datasets import fetch
+fetch('2017-05-22-brazilian-cities.csv', '../data')
+
+
+# In[11]:
+
+brazilian_cities = pd.read_csv('../data/2017-05-22-brazilian-cities.csv')
+brazilian_cities.head()
+
+
+# In[12]:
+
+brazilian_cities.shape
+
+
+# In[13]:
+
+brazilian_cities['state'] = brazilian_cities['state'].apply(lambda x: x.lower())
+
+
+# In[14]:
+
+import unicodedata
+
+def normalize_string(string):
+    if isinstance(string, str):
+        nfkd_form = unicodedata.normalize('NFKD', string.lower())
+        return nfkd_form.encode('ASCII', 'ignore').decode('utf-8')
+
+
+# In[15]:
+
+brazilian_cities['normalized_name'] = brazilian_cities['name'].apply(lambda x: normalize_string(x))
+brazilian_cities['normalized_name'] = brazilian_cities['normalized_name'].apply(lambda x: x.replace(' ', ''))
+
+
+# In[16]:
+
+brazilian_cities.head()
+
+
+# ## Getting all cities that are part of Transparency Portal
+# 
+# There are some cities that we already know that have a page with transparency and open data. The main object here is to find how many cities have that.
+# 
+# Pattern: `{city}-{state}.portaltp.com.br`
+
 # In[ ]:
 
+from requests import head
 
+def get_status(name, state):
+    return head('https://{}-{}.portaltp.com.br/'.format(name, state)).status_code
+    
+status = get_status('colatina', 'es')
 
+print(status)
 
-# In[ ]:
-
-
+brazilian_cities['status_portaltp'] = brazilian_cities[['normalized_name', 'state']].apply(lambda x: get_status(x['normalized_name'], x['state']))
 
