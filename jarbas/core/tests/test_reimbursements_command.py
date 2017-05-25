9c0f3c6969ca6597d12a9f8c55a4201aea534379
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-05-25 18:07:17 -0300

    Use update_or_create to handle reimbursement updates

--- jarbas/core/tests/test_reimbursements_command.py
+++ jarbas/core/tests/test_reimbursements_command.py
@@ -89,69 +89,53 @@ class TestSerializer(TestCommand):
         self.assertEqual(self.command.serialize(input), expected)
 
 
-class TestBulkCreate(TestCommand):
+class TestCreate(TestCommand):
 
-    @patch('jarbas.core.management.commands.reimbursements.Command.bulk_create')
-    def test_bulk_create_by(self, bulk_create):
-        self.command.bulk_create_by(range(0, 10), 3)
-        expected = (
-            call([0, 1, 2]),
-            call([3, 4, 5]),
-            call([6, 7, 8]),
-            call([9])
-        )
-        bulk_create.assert_has_calls(expected)
-
-    @patch.object(Reimbursement.objects, 'bulk_create')
+    @patch.object(Reimbursement.objects, 'update_or_create')
     @patch('jarbas.core.management.commands.reimbursements.Command.print_count')
-    def test_bulk_create(self, print_count, bulk_create):
-        self.command.count = 0
-        self.command.bulk_create([1, 2, 3])
-        bulk_create.assert_called_once_with([1, 2, 3])
-        self.assertEqual(1, print_count.call_count)
+    def test_create_or_update(self, print_count, create):
+        reimbursements = (dict(ahoy=42, document_id=1),)
+        self.command.create_or_update(reimbursements)
+        create.assert_called_once_with(
+            document_id=1,
+            defaults=dict(document_id=1, ahoy=42)
+        )
+        print_count.assert_called_once_with(Reimbursement, count=1)
 
 
 class TestConventionMethods(TestCommand):
 
     @patch('jarbas.core.management.commands.reimbursements.print')
     @patch('jarbas.core.management.commands.reimbursements.Command.reimbursements')
-    @patch('jarbas.core.management.commands.reimbursements.Command.bulk_create_by')
+    @patch('jarbas.core.management.commands.reimbursements.Command.create_or_update')
     @patch('jarbas.core.management.commands.reimbursements.Command.print_count')
-    def test_handler_without_options(self, print_count, bulk_create_by, reimbursements, print_):
+    def test_handler_without_options(self, print_count, create, reimbursements, print_):
         reimbursements.return_value = (1, 2, 3)
-        self.command.handle(dataset='reimbursements.xz', batch_size=42)
+        self.command.handle(dataset='reimbursements.xz')
         print_.assert_called_once_with('Starting with 0 reimbursements')
-        bulk_create_by.assert_called_once_with(reimbursements, 42)
+        create.assert_called_once_with(reimbursements)
         self.assertEqual('reimbursements.xz', self.command.path)
 
     @patch('jarbas.core.management.commands.reimbursements.print')
     @patch('jarbas.core.management.commands.reimbursements.Command.reimbursements')
-    @patch('jarbas.core.management.commands.reimbursements.Command.bulk_create_by')
+    @patch('jarbas.core.management.commands.reimbursements.Command.create_or_update')
     @patch('jarbas.core.management.commands.reimbursements.Command.drop_all')
     @patch('jarbas.core.management.commands.reimbursements.Command.print_count')
-    def test_handler_with_options(self, print_count, drop_all, bulk_create_by, reimbursements, print_):
-        self.command.handle(dataset='reimbursements.xz', batch_size=1, drop=True)
+    def test_handler_with_options(self, print_count, drop_all, create, reimbursements, print_):
+        self.command.handle(dataset='reimbursements.xz', drop=True)
         print_.assert_called_once_with('Starting with 0 reimbursements')
         drop_all.assert_called_once_with(Reimbursement)
-        bulk_create_by.assert_called_once_with(reimbursements, 1)
-
-    @patch('jarbas.core.management.commands.reimbursements.LoadCommand.add_arguments')
-    def test_add_arguments(self, super_add_arguments):
-        parser = MagicMock()
-        self.command.add_arguments(parser)
-        self.assertEqual(1, parser.add_argument.call_count)
-        super_add_arguments.assert_called_once_with(parser)
+        create.assert_called_once_with(reimbursements)
 
 
 class TestFileLoader(TestCommand):
 
     @patch('jarbas.core.management.commands.reimbursements.lzma')
     @patch('jarbas.core.management.commands.reimbursements.csv.DictReader')
-    @patch('jarbas.core.management.commands.reimbursements.Reimbursement')
     @patch('jarbas.core.management.commands.reimbursements.Command.serialize')
-    def test_reimbursement_property(self, serializer, reimbursement, row, lzma):
+    def test_reimbursement_property(self, serializer, row, lzma):
         lzma.return_value = StringIO()
         row.return_value = dict(ahoy=42)
         self.command.path = 'reimbursements.xz'
         list(self.command.reimbursements)
-        self.assertEqual(1, reimbursement.call_count)
+        self.assertEqual(1, serializer.call_count)
