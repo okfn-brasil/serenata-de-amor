2b1fd46854ae45532a5df2d1e23b5a0c126a5d9a
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-06-02 15:08:59 -0300

    Remove unused database indexes

--- jarbas/core/models.py
+++ jarbas/core/models.py
@@ -36,15 +36,15 @@ class Reimbursement(models.Model):
     applicant_id = models.IntegerField('Identificador do Solicitante', db_index=True)
 
     total_reimbursement_value = models.DecimalField('Valor da Restituição', max_digits=10, decimal_places=3, blank=True, null=True)
-    total_net_value = models.DecimalField('Valor Líquido', max_digits=10, decimal_places=3, db_index=True)
+    total_net_value = models.DecimalField('Valor Líquido', max_digits=10, decimal_places=3)
     reimbursement_numbers = models.CharField('Números dos Ressarcimentos', max_length=140)
     net_values = models.CharField('Valores Líquidos dos Ressarcimentos', max_length=140)
 
-    congressperson_id = models.IntegerField('Identificador Único do Parlamentar', db_index=True, blank=True, null=True)
+    congressperson_id = models.IntegerField('Identificador Único do Parlamentar', blank=True, null=True)
     congressperson_name = models.CharField('Nome do Parlamentar', max_length=140, db_index=True, blank=True, null=True)
     congressperson_document = models.IntegerField('Número da  Carteira Parlamentar', blank=True, null=True)
 
-    party = models.CharField('Partido', max_length=7, db_index=True, blank=True, null=True)
+    party = models.CharField('Partido', max_length=7, blank=True, null=True)
     state = models.CharField('UF', max_length=2, db_index=True, blank=True, null=True)
 
     term_id = models.IntegerField('Código da Legislatura', blank=True, null=True)
