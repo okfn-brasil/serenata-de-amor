67216e859bfb18bd305c6b79af3ad5761b9baca7
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-06-05 16:28:36 -0300

    Add dashboard widget for suspicions

--- jarbas/dashboard/admin.py
+++ jarbas/dashboard/admin.py
@@ -23,6 +23,38 @@ class ReceiptUrlWidget(Widget):
 
         url = '<div class="readonly"><a href="{}" target="_blank">{}</a></div>'
         return url.format(value, value)
+
+
+class SuspiciousWidget(Widget):
+
+    SUSPICIONS = (
+        'meal_price_outlier',
+        'over_monthly_subquota_limit',
+        'suspicious_traveled_speed_day',
+        'invalid_cnpj_cpf',
+        'election_expenses',
+        'irregular_companies_classifier'
+    )
+
+    HUMAN_NAMES = (
+        'Preço de refeição muito incomum',
+        'Extrapolou limita da (sub)quota',
+        'Muitas despesas em diferentes cidades no mesmo dia',
+        'CPF ou CNPJ inválidos',
+        'Gasto com campanha eleitoral',
+        'CNPJ irregular'
+    )
+
+    MAP = dict(zip(SUSPICIONS, HUMAN_NAMES))
+
+    def render(self, name, value, attrs=None, renderer=None):
+        value_as_dict = json.loads(value)
+        if not value_as_dict:
+            return ''
+
+        values = (self.MAP.get(k, k) for k in value_as_dict.keys())
+        suspicions = '<br>'.join(values)
+        return '<div class="readonly">{}</div>'.format(suspicions)
 class SuspiciousListFilter(SimpleListFilter):
 
     title = 'reembolso suspeito'
@@ -187,6 +219,7 @@ class ReimbursementModelAdmin(SimpleHistoryAdmin):
         if db_field.name in CUSTOM_WIDGETS:
             widgets = dict(
                 receipt_url=ReceiptUrlWidget,
+                suspicions=SuspiciousWidget
             )
             kwargs['widget'] = widgets.get(db_field.name)
         return super().formfield_for_dbfield(db_field, **kwargs)
