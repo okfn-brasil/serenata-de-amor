9cbc8b216bf088759aec3359e06cf49af50c8987
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-06-19 12:59:49 -0300

    Use django_extensions in debug mode

--- jarbas/settings.py
+++ jarbas/settings.py
@@ -182,5 +182,5 @@ CACHES = {
 if DEBUG:
     SKIP_TEMPLATE_PREFIXES = tuple()
     INTERNAL_IPS = ('127.0.0.1',)
-    INSTALLED_APPS = INSTALLED_APPS + ['debug_toolbar']
+    INSTALLED_APPS = INSTALLED_APPS + ['django_extensions', 'debug_toolbar']
     MIDDLEWARE = MIDDLEWARE + ['debug_toolbar.middleware.DebugToolbarMiddleware']
