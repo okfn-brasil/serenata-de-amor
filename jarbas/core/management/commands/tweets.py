0b6196deff112155350049acc34d379a0890f703
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-06-24 11:32:54 -0300

    Add docstrings (+minor refactor)

--- jarbas/core/management/commands/tweets.py
+++ jarbas/core/management/commands/tweets.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
             log.warning('No Twitter API consumer key and/or secret set.')
             return
 
-        # create API and pick the most recent Twiiter to limit the query
+        # connect to the API & pick the most recent Twiiter to limit the query
         self.api = twitter.Api(*credentials, sleep_on_rate_limit=True)
         self.latest_tweet = Tweet.objects.first()
 
@@ -45,13 +45,18 @@ class Command(BaseCommand):
             except Reimbursement.tweet.RelatedObjectDoesNotExist:
                 pass
             else:
-                if current_tweet and current_tweet.status == status:
+                if current_tweet.status == status:
                     continue
 
             self.save_tweet(reimbursement, status)
 
     @property
     def tweets(self):
+        """
+        Generator yielding the last tweets (200 is the API limit) since the
+        last tweet saved in Jarbas database. If no tweet is saved it yields the
+        last 200 tweets.
+        """
         kwargs = dict(
             screen_name='RosieDaSerenata',
             count=200,  # this is the maximum suported by Twitter API
@@ -66,17 +71,35 @@ class Command(BaseCommand):
 
     @property
     def urls(self):
+        """
+        Generator that iterates self.tweets and yields a tuple with the Twitter
+        status ID (int) and the URL (str) tweeted in that tweet. In case a
+        single tweet has more than one URL more than one tuple will be yielded
+        for that tweet.
+        """
         for tweet in self.tweets:
             yield from ((tweet.id, url.expanded_url) for url in tweet.urls)
 
     @staticmethod
     def get_document_id(url):
+        """
+        Gets a URL (str) and return the Reimbursement document ID (int)
+        contained in it. If nothing is found, or document ID is not an `int`
+        returns None.
+        """
         match = re.search(r'documentId\/(?P<document_id>\d*)', url)
-        if match:
-            return match.group('document_id')
+        try:
+            return int(match.group('document_id'))
+        except (AttributeError, ValueError):
+            return None  # AttributeError (no match) or ValueError (not an int)
 
     @property
     def document_ids(self):
+        """
+        Generator that consumes self.urls and yields a tuple with Tweet Status
+        ID (int) and Reimbursement document ID (int). It skips tweets URLs that
+        don't contain avalid reimbursement URL.
+        """
         for tweet_id, url in self.urls:
             document_id = self.get_document_id(url)
             if document_id:
@@ -84,6 +107,10 @@ class Command(BaseCommand):
 
     @staticmethod
     def save_tweet(reimbursement, status):
+        """
+        :param reimbursement: Reimbursement instance
+        :param status: (int) Tweet status ID
+        """
         reimbursement.tweet = Tweet(reimbursement=reimbursement, status=status)
         reimbursement.save()
 
