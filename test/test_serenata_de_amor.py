2b479927ee33181c57081df941bfdf347cd45423
Irio Musskopf <iirineu@gmail.com>
2017-07-23 02:38:05 +0300

    Verify existence of *.html and *.py versions for every notebook

--- test/test_serenata_de_amor.py
+++ test/test_serenata_de_amor.py
@@ -1,8 +1,23 @@
+import glob
+import subprocess
 from unittest import TestCase
 
 
 class TestSerenataDeAmor(TestCase):
 
-    def test_it_works(self):
-        self.assertEqual(4, 2 + 2)
-        self.assertNotEqual(5, 2 + 2)
+    def setUp(self):
+        self.notebook_files = glob.glob('develop/*.ipynb')
+
+    def test_html_versions_present(self):
+        """There is a *.html version of every Jupyter notebook."""
+        expected = [filename.replace('.ipynb', '.html')
+                    for filename in self.notebook_files]
+        html_files = glob.glob('develop/*.html')
+        self.assertEqual(expected, html_files)
+
+    def test_py_versions_present(self):
+        """There is a *.py version of every Jupyter notebook."""
+        expected = [filename.replace('.ipynb', '.py')
+                    for filename in self.notebook_files]
+        py_files = glob.glob('develop/*.py')
+        self.assertEqual(expected, py_files)
