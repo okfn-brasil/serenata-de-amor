20f0643dd169e2440bdebeb0f0ba79c9e03aaf09
Renne Rocha <renne@rennerocha.com>
2017-09-02 15:09:15 -0300

    Made irregular companies classifier tests more explicit

--- rosie/chamber_of_deputies/tests/test_irregular_companies_classifier.py
+++ rosie/chamber_of_deputies/tests/test_irregular_companies_classifier.py
@@ -1,6 +1,6 @@
+import datetime
 from unittest import TestCase
 
-import numpy as np
 import pandas as pd
 
 from rosie.chamber_of_deputies.classifiers.irregular_companies_classifier import IrregularCompaniesClassifier
@@ -9,30 +9,62 @@ from rosie.chamber_of_deputies.classifiers.irregular_companies_classifier import
 class TestIrregularCompaniesClassifier(TestCase):
 
     def setUp(self):
-        self.dataset = pd.read_csv('rosie/chamber_of_deputies/tests/fixtures/irregular_companies_classifier.csv',
-                                   dtype={'cnpj': np.str})
         self.subject = IrregularCompaniesClassifier()
 
+    def _get_company_dataset(self, fields):
+        base_company = {
+            'recipient_id': '02989654001197',
+            'situation_date': datetime.date(2013, 1, 3),
+            'issue_date': datetime.date(2013, 1, 30),
+            'situation': '',
+        }
+        for key, value in fields.items():
+            base_company[key] = value
+        dataset = pd.DataFrame([base_company, ])
+        return dataset
+
     def test_is_regular_company(self):
-        self.assertEqual(self.subject.predict(self.dataset)[0], False)
+        self.assertEqual(
+            self.subject.predict(self._get_company_dataset({
+                'situation': 'ABERTA'
+            }))[0], False)
 
     def test_is_irregular_company_BAIXADA(self):
-        self.assertEqual(self.subject.predict(self.dataset)[1], True)
+        self.assertEqual(
+            self.subject.predict(self._get_company_dataset({
+                'situation': 'BAIXADA'
+            }))[0], True)
 
     def test_is_irregular_company_NULA(self):
-        self.assertEqual(self.subject.predict(self.dataset)[2], True)
+        self.assertEqual(
+            self.subject.predict(self._get_company_dataset({
+                'situation': 'NULA'
+            }))[0], True)
 
     def test_is_irregular_company_INAPTA(self):
-        self.assertEqual(self.subject.predict(self.dataset)[3], True)
+        self.assertEqual(
+            self.subject.predict(self._get_company_dataset({
+                'situation': 'INAPTA'
+            }))[0], True)
 
     def test_is_irregular_company_SUSPENSA(self):
-        self.assertEqual(self.subject.predict(self.dataset)[4], True)
+        self.assertEqual(
+            self.subject.predict(self._get_company_dataset({
+                'situation': 'SUSPENSA'
+            }))[0], True)
 
-    def test_is_valid_reimbursement_based_on_date(self):
-        self.assertEqual(self.subject.predict(self.dataset)[5], False)
+    def test_is_valid_if_suspended_after_expense(self):
+        self.assertEqual(
+            self.subject.predict(self._get_company_dataset({
+                'situation': 'SUSPENSA',
+                'situation_date': datetime.date(2013, 1, 30),
+                'issue_date': datetime.date(2013, 1, 1),
+            }))[0], False)
 
-    def test_fit(self):
-        self.assertEqual(self.subject.fit(self.dataset), self.subject)
-
-    def test_transform(self):
-        self.assertEqual(self.subject.transform(), self.subject)
+    def test_is_irregular_if_suspended_before_expense(self):
+        self.assertEqual(
+            self.subject.predict(self._get_company_dataset({
+                'situation': 'SUSPENSA',
+                'situation_date': datetime.date(2013, 1, 1),
+                'issue_date': datetime.date(2013, 1, 30),
+            }))[0], True)
