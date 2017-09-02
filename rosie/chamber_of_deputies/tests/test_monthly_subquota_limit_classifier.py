b1d75c7d8e7d00fc77c87d3527271cfc18f3c7ce
Renne Rocha <renne@rennerocha.com>
2017-09-02 12:21:19 -0300

    Fixed PEP8 and renamed variable

--- rosie/chamber_of_deputies/tests/test_monthly_subquota_limit_classifier.py
+++ rosie/chamber_of_deputies/tests/test_monthly_subquota_limit_classifier.py
@@ -48,18 +48,19 @@ class TestMonthlySubquotaLimitClassifier(TestCase):
 
     MONTHLY_SUBQUOTA_LIMIT_FIXTURE_FILE = 'rosie/chamber_of_deputies/tests/fixtures/monthly_subquota_limit_classifier.csv'
 
-
     def setUp(self):
-        self.full_dataset = pd.read_csv(self.MONTHLY_SUBQUOTA_LIMIT_FIXTURE_FILE, dtype={'subquota_number': np.str})
-        self.dataset = self.full_dataset[['applicant_id', 'subquota_number', 'issue_date', 'year', 'month', 'net_value']]
-        self.result_dataset = self.full_dataset[['expected_prediction', 'test_case_description']]
+        self.full_dataset = pd.read_csv(
+            self.MONTHLY_SUBQUOTA_LIMIT_FIXTURE_FILE, dtype={'subquota_number': np.str})
+        self.dataset = self.full_dataset[
+            ['applicant_id', 'subquota_number', 'issue_date', 'year', 'month', 'net_value']]
+        self.test_result_dataset = self.full_dataset[['expected_prediction', 'test_case_description']]
 
         self.subject = MonthlySubquotaLimitClassifier()
         self.subject.fit_transform(self.dataset)
         self.prediction = self.subject.predict(self.dataset)
 
     def test_predictions(self):
-        for index, row in self.result_dataset.iterrows():
+        for index, row in self.test_result_dataset.iterrows():
             self.assertEqual(
                 self.prediction[index],
                 row['expected_prediction'],
