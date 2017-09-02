2140ef73009b070874bf3bbbbf5f1718e080c53d
Renne Rocha <renne@rennerocha.com>
2017-09-02 10:38:56 -0300

    Included expected result and test description in fixture to make it easier to include new test cases

--- rosie/chamber_of_deputies/tests/test_monthly_subquota_limit_classifier.py
+++ rosie/chamber_of_deputies/tests/test_monthly_subquota_limit_classifier.py
@@ -2,43 +2,26 @@ from unittest import TestCase
 
 import numpy as np
 import pandas as pd
-from numpy.testing import assert_array_equal
 
 from rosie.chamber_of_deputies.classifiers.monthly_subquota_limit_classifier import MonthlySubquotaLimitClassifier
 
 
 class TestMonthlySubquotaLimitClassifier(TestCase):
 
+    MONTHLY_SUBQUOTA_LIMIT_FIXTURE = 'rosie/chamber_of_deputies/tests/fixtures/monthly_subquota_limit_classifier.csv'
+
     def setUp(self):
-        self.dataset = pd.read_csv('rosie/chamber_of_deputies/tests/fixtures/monthly_subquota_limit_classifier.csv',
-                                   dtype={'subquota_number': np.str})
+        self.full_dataset = pd.read_csv(self.MONTHLY_SUBQUOTA_LIMIT_FIXTURE, dtype={'subquota_number': np.str})
+        self.dataset = self.full_dataset[['applicant_id', 'subquota_number', 'issue_date', 'year', 'month', 'net_value']]
+        self.result_dataset = self.full_dataset[['expected_prediction', 'test_case_description']]
+
         self.subject = MonthlySubquotaLimitClassifier()
         self.subject.fit_transform(self.dataset)
         self.prediction = self.subject.predict(self.dataset)
 
-    def test_predict_false_when_not_in_date_range(self):
-        assert_array_equal(np.repeat(False, 10),
-                           self.prediction[[0, 1, 9, 10, 18, 19, 27, 28, 36, 37]])
-
-    def test_predict_false_when_under_the_limit(self):
-        assert_array_equal(np.repeat(False, 10),
-                           self.prediction[[2, 3, 11, 12, 20, 21, 29, 30, 38, 39]])
-
-    def test_predict_false_when_exactly_on_the_limit(self):
-        assert_array_equal(np.repeat(False, 10),
-                           self.prediction[[4, 5, 13, 14, 22, 23, 31, 32, 40, 41]])
-
-    def test_predict_true_when_over_the_limit(self):
-        assert_array_equal(np.r_[[False, True, True]],
-                           self.prediction[[6, 7, 8]])
-        assert_array_equal(np.r_[[False, True, True]],
-                           self.prediction[[15, 16, 17]])
-        assert_array_equal(np.r_[[False, True, True]],
-                           self.prediction[[24, 25, 26]])
-        assert_array_equal(np.r_[[False, True, True]],
-                           self.prediction[[33, 34, 35]])
-        assert_array_equal(np.r_[[False, True, True]],
-                           self.prediction[[42, 43, 44]])
-
-    def test_predict_proba(self):
-        self.assertEqual(self.subject.predict_proba(), 1)
+    def test_predictions(self):
+        for index, row in self.result_dataset.iterrows():
+            self.assertEqual(
+                self.prediction[index],
+                row['expected_prediction'],
+                msg=row['test_case_description'])
