48aeac8184c541f16bd5ffefcea27e33221249a6
Giovani <sousa_giovani@hotmail.com>
2017-10-23 00:03:22 -0200

    Update app's verbose name

--- jarbas/chamber_of_deputies/app.py
+++ jarbas/chamber_of_deputies/app.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ChamberOfDeputiesConfig(AppConfig):
     name = 'jarbas.chamber_of_deputies'
-    verbose_name = 'Câmara dos Deputados'
+    verbose_name = 'Câmara dos Deputados - Cota para Exercício da Atividade Parlamentar'
