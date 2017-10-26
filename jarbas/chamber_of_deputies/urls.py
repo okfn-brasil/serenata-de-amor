ea125aa2a1d73ac9d2fb173a0b40e84fe537332f
Giovani <sousa_giovani@hotmail.com>
2017-10-26 11:02:18 -0200

    Update api/chamber_of_deputies urls and related

--- jarbas/chamber_of_deputies/urls.py
+++ jarbas/chamber_of_deputies/urls.py
@@ -12,25 +12,25 @@ from jarbas.chamber_of_deputies.views import (
 
 urlpatterns = [
     url(
-        r'^chamber_of_deputies/reimbursement/$',
+        r'^reimbursement/$',
         ReimbursementListView.as_view(),
         name='reimbursement-list'
     ),
     url(
-        r'^chamber_of_deputies/reimbursement/(?P<document_id>\d+)/$',
+        r'^reimbursement/(?P<document_id>\d+)/$',
         ReimbursementDetailView.as_view(),
         name='reimbursement-detail'
     ),
     url(
-        r'^chamber_of_deputies/reimbursement/(?P<document_id>\d+)/receipt/$',
+        r'^reimbursement/(?P<document_id>\d+)/receipt/$',
         ReceiptDetailView.as_view(),
         name='reimbursement-receipt'
     ),
     url(
-        r'^chamber_of_deputies/reimbursement/(?P<document_id>\d+)/same_day/$',
+        r'^reimbursement/(?P<document_id>\d+)/same_day/$',
         SameDayReimbursementListView.as_view(),
         name='reimbursement-same-day'
     ),
-    url(r'^chamber_of_deputies/applicant/$', ApplicantListView.as_view(), name='applicant-list'),
-    url(r'^chamber_of_deputies/subquota/$', SubquotaListView.as_view(), name='subquota-list')
+    url(r'^applicant/$', ApplicantListView.as_view(), name='applicant-list'),
+    url(r'^subquota/$', SubquotaListView.as_view(), name='subquota-list')
 ]
