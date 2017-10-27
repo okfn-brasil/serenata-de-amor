250022a82d531a45808c2c3ab3343d09ce083ded
Felipe de Morais <felipejpa15@gmail.com>
2017-10-27 01:05:11 -0200

    refactor test to just read from fixture file when necessary to improve test performance

--- rosie/chamber_of_deputies/tests/test_election_expenses_classifier.py
+++ rosie/chamber_of_deputies/tests/test_election_expenses_classifier.py
@@ -9,22 +9,27 @@ from rosie.chamber_of_deputies.classifiers import ElectionExpensesClassifier
 class TestElectionExpensesClassifier(TestCase):
 
     def setUp(self):
-        self.dataframe = pd.read_csv('rosie/chamber_of_deputies/tests/fixtures/election_expenses_classifier.csv',
-                                   dtype={'name': np.str, 'legal_entity': np.str})
         self.election_expenser_classifier = ElectionExpensesClassifier()
 
-    def test_is_election_company(self):
+    def test_legal_entity_is_a_election_company(self):
+        self.dataframe = pd.read_csv('rosie/chamber_of_deputies/tests/fixtures/election_expenses_classifier.csv',
+                                     dtype={'name': np.str, 'legal_entity': np.str})
+
         prediction_result = self.election_expenser_classifier.predict(self.dataframe)
 
         self.assertEqual(prediction_result[0], True)
 
-    def test_is_not_election_company(self):
+    def test_legal_entity_is_not_election_company(self):
+        self.dataframe = pd.read_csv('rosie/chamber_of_deputies/tests/fixtures/election_expenses_classifier.csv',
+                                      dtype={'name': np.str, 'legal_entity': np.str})
+
         prediction_result = self.election_expenser_classifier.predict(self.dataframe)
 
         self.assertEqual(prediction_result[1], False)
 
     def test_fit_just_for_formality_because_its_never_used(self):
-        self.assertTrue(self.election_expenser_classifier.fit(self.dataframe) is None)
+        empty_dataframe = pd.DataFrame()
+        self.assertTrue(self.election_expenser_classifier.fit(empty_dataframe) is None)
 
     def test_transform_just_for_formality_because_its_never_used(self):
         self.assertTrue(self.election_expenser_classifier.transform() is None)
