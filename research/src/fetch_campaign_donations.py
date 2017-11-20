86d4c5df21db07ab75e7eac9ed36d2799b4bf440
Eduardo Cuducos <cuducos@users.noreply.github.com>
2017-11-20 01:51:00 -0200

    Clean, normalize and translate column names
    Gather all these operations in a single point of the script

--- research/src/fetch_campaign_donations.py
+++ research/src/fetch_campaign_donations.py
@@ -11,11 +11,84 @@ from tqdm import tqdm
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(BASE_DIR, 'data')
+KEYS = ('candidates', 'parties', 'committees')
 YEARS = range(2010, 2017, 2)
+
+NORMALIZE_COLUMNS = {
+    'candidates': {
+        'Descricao da receita': 'Descrição da receita',
+        'Especie recurso': 'Espécie recurso',
+        'Numero candidato': 'Número candidato',
+        'Numero do documento': 'Número do documento',
+        'Numero Recibo Eleitoral': 'Número Recibo Eleitoral',
+        'Sigla  Partido': 'Sigla Partido'
+    },
+    'parties': {
+        'Sigla  Partido': 'Sigla Partido',
+        'Número recibo eleitoral': 'Número Recibo Eleitoral'
+    },
+    'committees': {
+        'Sigla  Partido': 'Sigla Partido',
+        'Tipo comite': 'Tipo Comite',
+        'Número recibo eleitoral': 'Número Recibo Eleitoral'
+    }
+}
+
+TRANSLATIONS = {
+    'Cargo': 'post',
+    'CNPJ Prestador Conta': 'accountable_company_id',
+    'Cod setor econômico do doador': 'donor_economic_setor_id',
+    'Cód. Eleição': 'election_id',
+    'CPF do candidato': 'candidate_cpf',
+    'CPF do vice/suplente': 'substitute_cpf',
+    'CPF/CNPJ do doador': 'donor_cnpj_or_cpf',
+    'CPF/CNPJ do doador originário':
+    'original_donor_cnpj_or_cpf',
+    'Data da receita': 'revenue_date',
+    'Data e hora': 'date_and_time',
+    'Desc. Eleição': 'election_description',
+    'Descrição da receita': 'revenue_description',
+    'Entrega em conjunto?': 'batch',
+    'Espécie recurso': 'type_of_revenue',
+    'Fonte recurso': 'source_of_revenue',
+    'Município': 'city',
+    'Nome candidato': 'candidate_name',
+    'Nome da UE': 'electoral_unit_name',
+    'Nome do doador': 'donor_name',
+    'Nome do doador (Receita Federal)':
+    'donor_name_for_federal_revenue',
+    'Nome do doador originário': 'original_donor_name',
+    'Nome do doador originário (Receita Federal)':
+    'original_donor_name_for_federal_revenue',
+    'Número candidato': 'candidate_number',
+    'Número candidato doador': 'donor_candidate_number',
+    'Número do documento': 'document_number',
+    'Número partido doador': 'donor_party_number',
+    'Número Recibo Eleitoral': 'electoral_receipt_number',
+    'Número UE': 'electoral_unit_number',
+    'Sequencial Candidato': 'candidate_sequence',
+    'Sequencial prestador conta': 'accountable_sequence',
+    'Sequencial comite': 'committee_sequence',
+    'Sequencial Diretorio': 'party_board_sequence',
+    'Setor econômico do doador': 'donor_economic_sector',
+    'Setor econômico do doador originário':
+    'original_donor_economic_sector',
+    'Sigla da UE': 'electoral_unit_abbreviation',
+    'Sigla Partido': 'party_acronym',
+    'Sigla UE doador': 'donor_electoral_unit_abbreviation',
+    'Tipo de documento': 'document_type',
+    'Tipo diretorio': 'party_board_type',
+    'Tipo doador originário': 'original_donor_type',
+    'Tipo partido': 'party_type',
+    'Tipo receita': 'revenue_type',
+    'Tipo comite': 'committee_type',
+    'UF': 'state',
+    'Valor receita': 'revenue_value'
+}
 
 
 class Donation:
-    """Context manager to download  a zip, read data them and cleanup"""
+    """Context manager to download, read data from a given year and cleanup"""
 
     URL = 'http://agencia.tse.jus.br/estatistica/sead/odsele/prestacao_contas'
 
@@ -87,7 +160,7 @@ class Donation:
         data = [self._read_csv(name) for name in self.path.glob(pattern)]
         return pd.concat(data)
 
-    def data(self):
+    def _data(self):
         """
         Returns a dictionary with data frames for candidates, parties and
         committees
@@ -101,11 +174,26 @@ class Donation:
             }
 
         paths = (os.path.join(self.directory, filename) for filename in files)
-        keys = ('candidates', 'parties', 'committees')
         return {
             key: self._read_csv(path, chunksize=10000)
-            for path, key in zip(keys, paths)
+            for path, key in zip(KEYS, paths)
         }
+
+    @property
+    def data(self):
+        """Takes self._data, clean, normalizes and translate it"""
+        data = self._data()
+        for key in KEYS:
+            normalize_columns = self.NORMALIZE_COLUMNS.get(key)
+            if key in data:
+                # strip columns names ('foobar ' -> 'foobar')
+                names = data[key].columns.values
+                cleaned_columns = {name: name.strip() for name in names}
+                data[key].rename(columns=cleaned_columns, inplace=True)
+                # normalize & translate
+                data[key].rename(columns=normalize_columns, inplace=True)
+                data[key].rename(columns=TRANSLATIONS, inplace=True)
+        return data
 
     def __enter__(self):
         self._download()
@@ -117,126 +205,14 @@ class Donation:
         shutil.rmtree(self.directory)
 
 
-def correct_columns(donations_data):
-    if 'candidates' in donations_data.keys():
-        donations_data['candidates'].rename(columns={'Descricao da receita':
-                                                     'Descrição da receita',
-                                                     'Especie recurso':
-                                                     'Espécie recurso',
-                                                     'Numero candidato':
-                                                     'Número candidato',
-                                                     'Numero do documento':
-                                                     'Número do documento',
-                                                     'Numero Recibo Eleitoral':
-                                                     'Número Recibo Eleitoral',
-                                                     'Sigla  Partido':
-                                                     'Sigla Partido'},
-                                            inplace=True)
-    if 'parties' in donations_data.keys():
-        donations_data['parties'].rename(columns={'Sigla  Partido':
-                                                  'Sigla Partido',
-                                                  'Número recibo eleitoral':
-                                                  'Número Recibo Eleitoral'},
-                                         inplace=True)
-    if 'committees' in donations_data.keys():
-        donations_data['committees'].rename(columns={'Sigla  Partido':
-                                                     'Sigla Partido',
-                                                     'Tipo comite':
-                                                     'Tipo Comite',
-                                                     'Número recibo eleitoral':
-                                                     'Número Recibo Eleitoral'},  # noqa
-                                            inplace=True)
-
-
-def translate_columns(donations_data):
-    translations = {'Cargo': 'post',
-                    'CNPJ Prestador Conta': 'accountable_company_id',
-                    'Cod setor econômico do doador': 'donor_economic_setor_id',
-                    'Cód. Eleição': 'election_id',
-                    'CPF do candidato': 'candidate_cpf',
-                    'CPF do vice/suplente': 'substitute_cpf',
-                    'CPF/CNPJ do doador': 'donor_cnpj_or_cpf',
-                    'CPF/CNPJ do doador originário':
-                    'original_donor_cnpj_or_cpf',
-                    'Data da receita': 'revenue_date',
-                    'Data e hora': 'date_and_time',
-                    'Desc. Eleição': 'election_description',
-                    'Descrição da receita': 'revenue_description',
-                    'Entrega em conjunto?': 'batch',
-                    'Espécie recurso': 'type_of_revenue',
-                    'Fonte recurso': 'source_of_revenue',
-                    'Município': 'city',
-                    'Nome candidato': 'candidate_name',
-                    'Nome da UE': 'electoral_unit_name',
-                    'Nome do doador': 'donor_name',
-                    'Nome do doador (Receita Federal)':
-                    'donor_name_for_federal_revenue',
-                    'Nome do doador originário': 'original_donor_name',
-                    'Nome do doador originário (Receita Federal)':
-                    'original_donor_name_for_federal_revenue',
-                    'Número candidato': 'candidate_number',
-                    'Número candidato doador': 'donor_candidate_number',
-                    'Número do documento': 'document_number',
-                    'Número partido doador': 'donor_party_number',
-                    'Número Recibo Eleitoral': 'electoral_receipt_number',
-                    'Número UE': 'electoral_unit_number',
-                    'Sequencial Candidato': 'candidate_sequence',
-                    'Sequencial prestador conta': 'accountable_sequence',
-                    'Sequencial comite': 'committee_sequence',
-                    'Sequencial Diretorio': 'party_board_sequence',
-                    'Setor econômico do doador': 'donor_economic_sector',
-                    'Setor econômico do doador originário':
-                    'original_donor_economic_sector',
-                    'Sigla da UE': 'electoral_unit_abbreviation',
-                    'Sigla Partido': 'party_acronym',
-                    'Sigla UE doador': 'donor_electoral_unit_abbreviation',
-                    'Tipo de documento': 'document_type',
-                    'Tipo diretorio': 'party_board_type',
-                    'Tipo doador originário': 'original_donor_type',
-                    'Tipo partido': 'party_type',
-                    'Tipo receita': 'revenue_type',
-                    'Tipo comite': 'committee_type',
-                    'UF': 'state',
-                    'Valor receita': 'revenue_value'}
-    if 'candidates' in donations_data.keys():
-        donations_data['candidates'].rename(columns=translations,
-                                            inplace=True)
-    if 'parties' in donations_data.keys():
-        donations_data['parties'].rename(columns=translations,
-                                         inplace=True)
-    if 'committees' in donations_data.keys():
-        donations_data['committees'].rename(columns=translations,
-                                            inplace=True)
-
-
-def strip_columns_names(donations_data, key):
-    if key in donations_data.keys():
-        columns_candidates = donations_data[key].columns.values
-        columns_mod_candidates = {}
-        for name in columns_candidates:
-            columns_mod_candidates[name] = name.strip()
-        donations_data[key].rename(columns=columns_mod_candidates,
-                                   inplace=True)
-
-
-def data_by_year(year):
-    with Donation(year) as donation:
-        data = donation.data()
-        for key in ('candidates', 'parties', 'committees'):
-            strip_columns_names(data, key)
-        return data
-
-
 if __name__ == '__main__':
-    donations_data = [data_by_year(year) for year in YEARS]
+    donations_data = [Donation(year).data for year in YEARS]
 
     donations_candidates_concatenated = []
     donations_parties_concatenated = []
     donations_committees_concatenated = []
 
     for data in donations_data:
-        correct_columns(data)
-        translate_columns(data)
         if 'candidates' in data.keys():
             donations_candidates_concatenated.append(data['candidates'])
         if 'parties' in data.keys():
