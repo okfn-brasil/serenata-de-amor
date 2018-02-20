e07b3e3e44ddd43d49b488cf7597e5daea95caa5
Irio <irio@Irios-MacBook-Pro.local>
2018-02-20 14:56:41 -0300

    Use NewRelic in Django project

--- jarbas/wsgi.py
+++ jarbas/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jarbas.settings")
 
+
+import newrelic.agent
+newrelic.agent.initialize()
+
 application = get_wsgi_application()
