9fa0cc695a870864b569ba1346af63f0c8e6bca7
Eduardo Cuducos <cuducos@users.noreply.github.com>
2018-09-13 10:52:47 -0300

    Fix tests and minor bugs

--- jarbas/settings.py
+++ jarbas/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = config('STATICFILES_STORAGE', default='whitenoise.storage.CompressedManifestStaticFilesStorage')
+STATICFILES_STORAGE = config('STATICFILES_STORAGE', default='django.contrib.staticfiles.storage.StaticFilesStorage')
 
 # Django REST Framework
 
