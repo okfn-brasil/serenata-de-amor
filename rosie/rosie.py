37cf9e38c37c22d21487b7f4c0d70856ce9ddbed
Eduardo Cuducos <cuducos@users.noreply.github.com>
2018-12-20 15:37:10 -0200

    Use docopt to refactor Rosie's CLI

--- rosie/rosie.py
+++ rosie/rosie.py
@@ -1,29 +1,63 @@
-#!/usr/bin/env python
+"""
+Hi, I am Rosie from Operação Serenata de Amor! 🤖
 
-from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+I'm a proof-of-concept for the usage of artificial intelligence for social
+control of public administration.
 
-from rosie import chamber_of_deputies, federal_senate
+Usage:
+  rosie.py run (chamber_of_deputies|federal_senate) [--output=<directory>]
+  rosie.py test [chamber_of_deputies|federal_senate|core]
+
+Options:
+  --help                Show this screen
+  --output=<directory>  Output directory [default: /tmp/serenata-data]
+"""
+import os
+import unittest
+
+from docopt import docopt
+
+import rosie
+import rosie.chamber_of_deputies
+import rosie.federal_senate
+
+
+def get_module(arguments):
+    modules = ('chamber_of_deputies', 'federal_senate', 'core')
+    for module in modules:
+        if arguments[module]:
+            return module
+
+
+def run(module, directory):
+    module = getattr(rosie, module)
+    module.main(directory)
+
+
+def test(module=None):
+    loader = unittest.TestLoader()
+    tests_path = 'rosie'
+
+    if module:
+        tests_path = os.path.join(tests_path, module)
+
+    tests = loader.discover(tests_path)
+    testRunner = unittest.runner.TextTestRunner()
+    result = testRunner.run(tests)
+    if not result.wasSuccessful():
+        exit(1)
 
 
 def main():
-    parser = ArgumentParser(description='''Artificial Intelligence for social control of public administration''',
-            epilog='''Example: rosie.py chamber-of-deputies''',
-        formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('module', nargs='?', default='all',
-        help='a module to run, could be chamber-of-deputies or federal-senate')
-    parser.add_argument('--dataset', '-d', nargs='?',
-        help='the source dataset')
-    args = parser.parse_args()
-    if ('chamber-of-deputies' in args.module) or args.module == 'all':
-        if args.dataset:
-            chamber_of_deputies.main(args.dataset)
-        else:
-            chamber_of_deputies.main()
-    if ('federal-senate' in args.module) or args.module == 'all':
-        if args.dataset:
-            federal_senate.main(args.dataset)
-        else:
-            federal_senate.main()
+    arguments = docopt(__doc__)
+    module = get_module(arguments)
+
+    if arguments['test']:
+        test(module)
+
+    if arguments['run']:
+        module = module if module != 'core' else None
+        run(module, arguments['--output'])
 
 
 if __name__ == '__main__':
