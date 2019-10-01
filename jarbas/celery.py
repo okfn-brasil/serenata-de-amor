eb7d01ce4d7efbd55db4c91d27076b4188da2d36
Oleg Utkin <oleg_utkin@me.com>
2019-10-02 00:16:09 +0300

    schedule searchvector command

--- jarbas/celery.py
+++ jarbas/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jarbas.settings')
@@ -7,3 +8,17 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jarbas.settings')
 app = Celery('jarbas')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    from django.core import management
+
+    @app.task(ignore_result=True)
+    def searchvector():
+        management.call_command('searchvector')
+
+    sender.add_periodic_task(
+        crontab(minute='0', hour='2', day_of_month='*/2'),
+        searchvector.s(),
+    )
