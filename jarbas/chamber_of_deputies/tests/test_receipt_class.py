cdceafc0730d3f37fc49d9ad65f1ecebd007f4e9
Giovani <sousa_giovani@hotmail.com>
2019-10-15 22:36:37 -0300

    Fix receipt url for eletronic receipts

--- jarbas/chamber_of_deputies/tests/test_receipt_class.py
+++ jarbas/chamber_of_deputies/tests/test_receipt_class.py
@@ -9,11 +9,17 @@ from jarbas.chamber_of_deputies.models import Receipt
 class TestReceipt(TestCase):
 
     def setUp(self):
-        self.receipt = Receipt(1970, 13, 42)
+        self.receipt = Receipt(1970, 13, 42, 1)
+        self.eletronic_receipt = Receipt(1970, 13, 42, 4)
 
     def test_url(self):
         expected = 'http://www.camara.gov.br/cota-parlamentar/documentos/publ/13/1970/42.pdf'
         self.assertEqual(expected, self.receipt.url)
+
+    def test_eletronic_url(self):
+        expected = ('https://www.camara.leg.br/cota-parlamentar/nota-fiscal-eletronica?'
+                    'ideDocumentoFiscal=42')
+        self.assertEqual(expected, self.eletronic_receipt.url)
 
     @patch('jarbas.chamber_of_deputies.models.head')
     def test_existing_url(self, mocked_head):
