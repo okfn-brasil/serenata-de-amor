59b3c5eb10e900cd709d2cc4fcb9c3894b03d4ad
Mario Sergio <mariopekenossalro@gmail.com>
2020-07-15 14:48:34 -0300

    new branch default: main

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,6 +1,6 @@
 export CURRENT_DIR=$(pwd)
 cd /opt/serenata-de-amor
-git pull origin master
+git pull origin main
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml run --rm django python manage.py collectstatic --no-input
